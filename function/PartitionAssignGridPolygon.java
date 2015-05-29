27494df67cd919adb8cf4a6c45a5464e4bced352
Jia Yu <jiayu2@asu.edu>
2015-05-29 10:50:11 -0700

    One bug fixed
    Fix this bug: Did not consider one possible relation between two spatial objects in spatial join: When one grid cell is very small, it might be contained by the rectangle or polygon. So far, the spatial join considers four relations between two spatial objects: fully contain, be fully contained, overlap and disjoint.

--- function/PartitionAssignGridPolygon.java
+++ function/PartitionAssignGridPolygon.java
@@ -8,6 +8,7 @@ import org.apache.spark.api.java.function.PairFlatMapFunction;
 
 import scala.Tuple2;
 
+import com.vividsolutions.jts.geom.Envelope;
 import com.vividsolutions.jts.geom.Polygon;
 import com.vividsolutions.jts.geom.Polygon;
 
@@ -28,37 +29,23 @@ public class PartitionAssignGridPolygon implements PairFlatMapFunction<java.util
 
 			public Iterable<Tuple2<Integer, Polygon>> call(Iterator<Polygon> s) throws Exception 	
 			{
-				int id=-1;
 				ArrayList<Tuple2<Integer, Polygon>> list=new ArrayList<Tuple2<Integer, Polygon>>();
 				
 				while(s.hasNext())
 				{
 							
 					Polygon currentElement=s.next();
-							for(int i=0;i<gridNumberHorizontal;i++)
+					Integer id=0;
+					for(int j=0;j<gridNumberVertical;j++)
 							{
-								for(int j=0;j<gridNumberVertical;j++)
+								for(int i=0;i<gridNumberHorizontal;i++)
 								{
-									Boolean Vertex1Condition=currentElement.getEnvelopeInternal().getMinX()>=gridHorizontalBorder[i] && currentElement.getEnvelopeInternal().getMinX()<=gridHorizontalBorder[i+1]&&currentElement.getEnvelopeInternal().getMaxX()>=gridHorizontalBorder[i] && currentElement.getEnvelopeInternal().getMaxX()<=gridHorizontalBorder[i+1] ;
-									Boolean Vertex2Condition=currentElement.getEnvelopeInternal().getMinY()>=gridVerticalBorder[j] && currentElement.getEnvelopeInternal().getMinY()<=gridVerticalBorder[j+1]&&currentElement.getEnvelopeInternal().getMaxY()>=gridVerticalBorder[j] && currentElement.getEnvelopeInternal().getMaxY()<=gridVerticalBorder[j+1];
-									
-									if(Vertex1Condition && Vertex2Condition )
+									Envelope currentGrid=new Envelope(gridHorizontalBorder[i],gridHorizontalBorder[i+1],gridVerticalBorder[j],gridVerticalBorder[j+1]);
+									if(currentGrid.intersects(currentElement.getEnvelopeInternal())||currentGrid.contains(currentElement.getEnvelopeInternal())||currentElement.getEnvelopeInternal().contains(currentGrid))
 									{
-										//Fully contain
-										id=i*gridNumberHorizontal+j;
 										list.add(new Tuple2(id,currentElement));
 									}
-									else if(!Vertex1Condition && !Vertex2Condition)
-										{
-											//Fully disjoint
-											continue;
-										}
-										else
-										{
-											//Overlap
-											id=i*gridNumberHorizontal+j;
-											list.add(new Tuple2(id,currentElement));
-										}
+									id++;
 								}
 							}
 				}
