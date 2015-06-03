8b939d81c448ff39ac015b7461369b3550d2e6b8
Jia Yu <jiayu2@asu.edu>
2015-06-02 18:39:39 -0700

    Add FlatMap for sparse matrix in spatial analysis

--- SpatialPairRDD.java
+++ SpatialPairRDD.java
@@ -2,9 +2,15 @@ package GeoSpark;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Iterator;
 
 import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.function.PairFlatMapFunction;
 import org.apache.spark.api.java.function.PairFunction;
+
+import com.vividsolutions.jts.geom.Envelope;
+import com.vividsolutions.jts.geom.Point;
+import com.vividsolutions.jts.geom.Polygon;
 
 import scala.Tuple2;
 
@@ -40,7 +46,67 @@ public class SpatialPairRDD<T1,T2> implements Serializable{
 	{
 		this.getSpatialPairRDD().saveAsTextFile(OutputLocation);
 	}
-	public SpatialPairRDD<T1, Integer> countByKey() throws Exception
+	public SpatialPairRDD<T1,Point> FlatMapToPoint()
+	{
+		JavaPairRDD<T1,ArrayList<Point>> spatialPairRDDTemp=(JavaPairRDD<T1, ArrayList<Point>>) this.spatialPairRDD;
+		SpatialPairRDD<T1,Point> spatialPairRDDflat=new SpatialPairRDD<T1,Point>(spatialPairRDDTemp.flatMapToPair(new PairFlatMapFunction<Tuple2<T1,ArrayList<Point>>,T1,Point>()
+				{
+
+					public Iterable<Tuple2<T1, Point>> call(
+							Tuple2<T1, ArrayList<Point>> t) {
+						ArrayList<Tuple2<T1,Point>> result=new ArrayList<Tuple2<T1,Point>>();
+						Iterator<Point> targetIterator=t._2().iterator();
+						while(targetIterator.hasNext())
+						{
+							result.add(new Tuple2<T1,Point>(t._1(),targetIterator.next()));
+						}
+						return result;
+					}
+			
+				}));
+		return spatialPairRDDflat;
+	}
+	public SpatialPairRDD<T1,Envelope> FlatMapToRectangle()
+	{
+		JavaPairRDD<T1,ArrayList<Envelope>> spatialPairRDDTemp=(JavaPairRDD<T1, ArrayList<Envelope>>) this.spatialPairRDD;
+		SpatialPairRDD<T1,Envelope> spatialPairRDDflat=new SpatialPairRDD<T1,Envelope>(spatialPairRDDTemp.flatMapToPair(new PairFlatMapFunction<Tuple2<T1,ArrayList<Envelope>>,T1,Envelope>()
+				{
+
+					public Iterable<Tuple2<T1, Envelope>> call(
+							Tuple2<T1, ArrayList<Envelope>> t) {
+						ArrayList<Tuple2<T1,Envelope>> result=new ArrayList<Tuple2<T1,Envelope>>();
+						Iterator<Envelope> targetIterator=t._2().iterator();
+						while(targetIterator.hasNext())
+						{
+							result.add(new Tuple2<T1,Envelope>(t._1(),targetIterator.next()));
+						}
+						return result;
+					}
+			
+				}));
+		return spatialPairRDDflat;
+	}
+	public SpatialPairRDD<T1,Polygon> FlatMapToPolygon()
+	{
+		JavaPairRDD<T1,ArrayList<Polygon>> spatialPairRDDTemp=(JavaPairRDD<T1, ArrayList<Polygon>>) this.spatialPairRDD;
+		SpatialPairRDD<T1,Polygon> spatialPairRDDflat=new SpatialPairRDD<T1,Polygon>(spatialPairRDDTemp.flatMapToPair(new PairFlatMapFunction<Tuple2<T1,ArrayList<Polygon>>,T1,Polygon>()
+				{
+
+					public Iterable<Tuple2<T1, Polygon>> call(
+							Tuple2<T1, ArrayList<Polygon>> t) {
+						ArrayList<Tuple2<T1,Polygon>> result=new ArrayList<Tuple2<T1,Polygon>>();
+						Iterator<Polygon> targetIterator=t._2().iterator();
+						while(targetIterator.hasNext())
+						{
+							result.add(new Tuple2<T1,Polygon>(t._1(),targetIterator.next()));
+						}
+						return result;
+					}
+			
+				}));
+		return spatialPairRDDflat;
+	}
+	public SpatialPairRDD<T1, Integer> countByKey()
 	{
 			JavaPairRDD<T1,ArrayList> spatialPairRDDTemp=(JavaPairRDD<T1, ArrayList>) this.spatialPairRDD;
 			SpatialPairRDD<T1,Integer> spatialPairRDDcount= new SpatialPairRDD<T1,Integer> (spatialPairRDDTemp.mapToPair(new PairFunction<Tuple2<T1,ArrayList>,T1,Integer>()
