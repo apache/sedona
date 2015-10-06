3b15bb4f816516842718293964478bf3f06661ba
jinxuan wu <jinxuanw@asu.edu>
2015-10-05 20:25:22 -0700

    Finish refactoring GemotryComparatorFactory

--- src/main/java/org/datasyslab/geospark/utils/GeometryComparatorFactory.java
+++ src/main/java/org/datasyslab/geospark/utils/GeometryComparatorFactory.java
@@ -39,18 +39,45 @@ public class GeometryComparatorFactory {
 		return comp;
 	}
 	
+	/**
+	 * Creates a new Comparator object.
+	 *
+	 * @param Two strings as input, should be in format like ("point", "x");
+	 * @param axis the axis
+	 * @param extrema, whether we want the max or the min
+	 * @return the gemotry comparator
+	 */
 	public static GemotryComparator createComparator(String gemotryType, String axis, String extrema){
 		GemotryComparator comp = null;
 		switch(gemotryType.toUpperCase()) {
 			case "CIRCLE":
-				if(axis.toUpperCase().equals("X"))
-					comp = new PointXComparator();
+				if(axis.toUpperCase().equals("X")) {
+					return extrema.toUpperCase().equals("MAX")?new CircleXMaxComparator():new CircleXMinComparator();
+				}
+				else if (axis.toUpperCase().equals("Y")){
+					return extrema.toUpperCase().equals("MAX")?new CircleYMaxComparator():new CircleYMinComparator();
+				}
+				
 				break;
 			case "POLYGON":
+				if(axis.toUpperCase().equals("X")) {
+					return extrema.toUpperCase().equals("MAX")?new PolygonXMaxComparator():new PolygonXMinComparator();
+				}
+				else if (axis.toUpperCase().equals("Y")){
+					return extrema.toUpperCase().equals("MAX")?new PolygonYMaxComparator():new PolygonYMinComparator();
+				}
 				break;
 			case "RECTANGLE":
+				if(axis.toUpperCase().equals("X")) {
+					return extrema.toUpperCase().equals("MAX")?new RectangleXMaxComparator():new RectangleXMinComparator();
+				}
+				else if (axis.toUpperCase().equals("Y")){
+					return extrema.toUpperCase().equals("MAX")?new RectangleYMaxComparator():new RectangleYMinComparator();
+				}
 				break;
-
+			default:
+				//TODO: add exception later
+				break;
 		}
 		return comp;
 	}
