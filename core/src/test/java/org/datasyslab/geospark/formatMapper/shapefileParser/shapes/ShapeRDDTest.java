8e4b7f70f7a97c7f9f706093e3a7eec93094cccd
zongsizhang <krishnazongsi@gmail.com>
2017-06-13 13:55:03 -0700

    add test for shapeRDD

---
+++ core/src/test/java/org/datasyslab/geospark/formatMapper/shapefileParser/shapes/ShapeRDDTest.java
@@ -0,0 +1,23 @@
+package org.datasyslab.geospark.formatMapper.shapefileParser.shapes;
+
+import org.junit.After;
+import org.junit.Before;
+
+import java.io.Serializable;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by zongsizhang on 6/13/17.
+ */
+public class ShapeRDDTest implements Serializable{
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+}
