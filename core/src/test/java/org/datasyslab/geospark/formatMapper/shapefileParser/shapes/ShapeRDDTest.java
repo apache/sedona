0b96eb61ce903aa3165365fd62fe250090e30ea9
zongsizhang <krishnazongsi@gmail.com>
2017-06-13 15:23:07 -0700

    add ShapeRDDTest, add test function verify count of shapes loaded

--- core/src/test/java/org/datasyslab/geospark/formatMapper/shapefileParser/shapes/ShapeRDDTest.java
+++ core/src/test/java/org/datasyslab/geospark/formatMapper/shapefileParser/shapes/ShapeRDDTest.java
@@ -1,9 +1,24 @@
 package org.datasyslab.geospark.formatMapper.shapefileParser.shapes;
 
-import org.junit.After;
-import org.junit.Before;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.geotools.data.DataStore;
+import org.geotools.data.DataStoreFinder;
+import org.geotools.data.FeatureSource;
+import org.geotools.feature.FeatureCollection;
+import org.junit.*;
+import org.opengis.feature.simple.SimpleFeature;
+import org.opengis.feature.simple.SimpleFeatureType;
+import org.opengis.filter.Filter;
 
+import java.io.File;
+import java.io.IOException;
 import java.io.Serializable;
+import java.net.MalformedURLException;
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.junit.Assert.*;
 
@@ -11,9 +26,38 @@ import static org.junit.Assert.*;
  * Created by zongsizhang on 6/13/17.
  */
 public class ShapeRDDTest implements Serializable{
-    @Before
-    public void setUp() throws Exception {
 
+    /** The sc. */
+    public static JavaSparkContext sc;
+
+    /** The Input location. */
+    public static String InputLocation;
+
+    @BeforeClass
+    public static void onceExecutedBeforeAll() {
+        SparkConf conf = new SparkConf().setAppName("ShapeRDDTest").setMaster("local[2]").set("spark.executor.cores","2");
+        sc = new JavaSparkContext(conf);
+        Logger.getLogger("org").setLevel(Level.WARN);
+        Logger.getLogger("akka").setLevel(Level.WARN);
+        //Hard code to a file in resource folder. But you can replace it later in the try-catch field in your hdfs system.
+        InputLocation = ShapeRDDTest.class.getClassLoader().getResource("shapefiles/polygon").getPath();
+    }
+
+    @Test
+    public void testLoadShapeFile() throws IOException {
+        // load shape with geotool.shapefile
+        File file = new File(InputLocation);
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("url", file.toURI().toURL());
+        DataStore dataStore = DataStoreFinder.getDataStore(map);
+        String typeName = dataStore.getTypeNames()[0];
+        FeatureSource<SimpleFeatureType, SimpleFeature> source = dataStore
+                .getFeatureSource(typeName);
+        Filter filter = Filter.INCLUDE;
+        FeatureCollection<SimpleFeatureType, SimpleFeature> collection = source.getFeatures(filter);
+        // load shapes with our tool
+        ShapeRDD shapeRDD = new ShapeRDD(InputLocation,sc);
+        Assert.assertEquals(shapeRDD.getShapeWritableRDD().collect().size(), collection.size());
     }
 
     @After
