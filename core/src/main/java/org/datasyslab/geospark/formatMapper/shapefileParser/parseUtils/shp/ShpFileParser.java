44ec302d8765e72148545ccfcdabc9c485f3daff
zongsi zhang <krishnazongsi@gmail.com>
2017-07-28 15:34:18 -0700

    clear print

--- core/src/main/java/org/datasyslab/geospark/formatMapper/shapefileParser/parseUtils/shp/ShpFileParser.java
+++ core/src/main/java/org/datasyslab/geospark/formatMapper/shapefileParser/parseUtils/shp/ShpFileParser.java
@@ -70,7 +70,6 @@ public class ShpFileParser implements Serializable, ShapeFileConst{
         // get length of record content
         int contentLength = reader.readInt();
         long recordLength = 16 * (contentLength + 4);
-        System.out.println(contentLength * 2 - INT_LENGTH + " =========================");
         remainLength -= recordLength;
         int typeID = EndianUtils.swapInteger(reader.readInt());
         byte[] contentArray = new byte[contentLength * 2 - INT_LENGTH];// exclude the 4 bytes we read for shape type
