6417e9a03f991483421f5d41bb584de8887bf846
Masha Basmanova <mbasmanova@fb.com>
2017-10-03 14:47:54 -0400

    Added tests for BoundaryAggregation

--- core/src/test/java/org/datasyslab/geospark/spatialRDD/PointRDDTest.java
+++ core/src/test/java/org/datasyslab/geospark/spatialRDD/PointRDDTest.java
@@ -6,118 +6,37 @@
  */
 package org.datasyslab.geospark.spatialRDD;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.Serializable;
-import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
-import java.util.Properties;
-
-import org.apache.log4j.Level;
-import org.apache.log4j.Logger;
-import org.apache.spark.SparkConf;
-import org.apache.spark.api.java.JavaSparkContext;
+import com.vividsolutions.jts.geom.Envelope;
+import com.vividsolutions.jts.geom.Point;
+import com.vividsolutions.jts.index.quadtree.Quadtree;
+import com.vividsolutions.jts.index.strtree.STRtree;
 import org.apache.spark.storage.StorageLevel;
-import org.datasyslab.geospark.enums.FileDataSplitter;
 import org.datasyslab.geospark.enums.GridType;
 import org.datasyslab.geospark.enums.IndexType;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import com.vividsolutions.jts.geom.Envelope;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
 
 /**
- * 
  * @author Arizona State University DataSystems Lab
- *
  */
-
-import com.vividsolutions.jts.geom.Point;
-import com.vividsolutions.jts.index.quadtree.Quadtree;
-import com.vividsolutions.jts.index.strtree.STRtree;
 
 // TODO: Auto-generated Javadoc
 /**
  * The Class PointRDDTest.
  */
-public class PointRDDTest implements Serializable{
-    
-    /** The sc. */
-    public static JavaSparkContext sc;
-    
-    /** The prop. */
-    static Properties prop;
-    
-    /** The input. */
-    static InputStream input;
-    
-    /** The Input location. */
-    static String InputLocation;
-    
-    /** The offset. */
-    static Integer offset;
-    
-    /** The splitter. */
-    static FileDataSplitter splitter;
-    
-    /** The grid type. */
-    static GridType gridType;
-    
-    /** The index type. */
-    static IndexType indexType;
-    
-    /** The num partitions. */
-    static Integer numPartitions;
-    
+public class PointRDDTest extends SpatialRDDTestBase
+{
     /**
      * Once executed before all.
      */
     @BeforeClass
     public static void onceExecutedBeforeAll() {
-        SparkConf conf = new SparkConf().setAppName("PointRDDTest").setMaster("local[2]");
-        sc = new JavaSparkContext(conf);
-        Logger.getLogger("org").setLevel(Level.WARN);
-        Logger.getLogger("akka").setLevel(Level.WARN);
-        prop = new Properties();
-        input = PointRDDTest.class.getClassLoader().getResourceAsStream("point.test.properties");
-
-        //Hard code to a file in resource folder. But you can replace it later in the try-catch field in your hdfs system.
-        InputLocation = "file://"+PointRDDTest.class.getClassLoader().getResource("primaryroads.csv").getPath();
-
-        offset = 0;
-        splitter = null;
-        gridType = null;
-        indexType = null;
-        numPartitions = 0;
-
-        try {
-            // load a properties file
-            prop.load(input);
-            // There is a field in the property file, you can edit your own file location there.
-            // InputLocation = prop.getProperty("inputLocation");
-            InputLocation = "file://"+PointRDDTest.class.getClassLoader().getResource(prop.getProperty("inputLocation")).getPath();
-            
-            offset = Integer.parseInt(prop.getProperty("offset"));
-            splitter = FileDataSplitter.getFileDataSplitter(prop.getProperty("splitter"));
-            gridType = GridType.getGridType(prop.getProperty("gridType"));
-            indexType = IndexType.getIndexType(prop.getProperty("indexType"));
-            
-            
-
-            numPartitions = Integer.parseInt(prop.getProperty("numPartitions"));
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        } finally {
-            if (input != null) {
-                try {
-                    input.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
+        initialize(PointRDDTest.class.getSimpleName(), "point.test.properties");
     }
 
     /**
@@ -131,9 +50,8 @@ public class PointRDDTest implements Serializable{
     @Test
     public void testConstructor() throws Exception {
         PointRDD spatialRDD = new PointRDD(sc, InputLocation, offset, splitter,true, numPartitions,StorageLevel.MEMORY_ONLY());
-        //todo: Set this to debug level
-        assert spatialRDD.approximateTotalCount>=1;
-        assert spatialRDD.boundaryEnvelope!=null;
+        assertEquals(inputCount, spatialRDD.approximateTotalCount);
+        assertEquals(inputBoundary, spatialRDD.boundaryEnvelope);
     }
 
     /**
---
+++ core/src/test/java/org/datasyslab/geospark/spatialRDD/SpatialRDDTestBase.java
@@ -0,0 +1,96 @@
+package org.datasyslab.geospark.spatialRDD;
+
+import com.vividsolutions.jts.geom.Envelope;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.datasyslab.geospark.enums.FileDataSplitter;
+import org.datasyslab.geospark.enums.GridType;
+import org.datasyslab.geospark.enums.IndexType;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class SpatialRDDTestBase
+{
+    /** The sc. */
+    public static JavaSparkContext sc;
+
+    /** The prop. */
+    static Properties prop;
+
+    /** The input. */
+    static InputStream input;
+
+    protected static long inputCount;
+    protected static Envelope inputBoundary;
+
+    /** The Input location. */
+    static String InputLocation;
+
+    /** The offset. */
+    static Integer offset;
+
+    /** The splitter. */
+    static FileDataSplitter splitter;
+
+    /** The grid type. */
+    static GridType gridType;
+
+    /** The index type. */
+    static IndexType indexType;
+
+    /** The num partitions. */
+    static Integer numPartitions;
+
+    /**
+     * Once executed before all.
+     */
+    protected static void initialize(final String testSuiteName, final String propertiesFileName) {
+        SparkConf conf = new SparkConf().setAppName(testSuiteName).setMaster("local[2]");
+        sc = new JavaSparkContext(conf);
+        Logger.getLogger("org").setLevel(Level.WARN);
+        Logger.getLogger("akka").setLevel(Level.WARN);
+        prop = new Properties();
+        ClassLoader classLoader = SpatialRDDTestBase.class.getClassLoader();
+        input = classLoader.getResourceAsStream(propertiesFileName);
+
+        offset = 0;
+        splitter = null;
+        gridType = null;
+        indexType = null;
+        numPartitions = 0;
+
+        try {
+            // load a properties file
+            prop.load(input);
+            // There is a field in the property file, you can edit your own file location there.
+            // InputLocation = prop.getProperty("inputLocation");
+            InputLocation = "file://"+ classLoader.getResource(prop.getProperty("inputLocation")).getPath();
+            inputCount = Long.parseLong(prop.getProperty("inputCount"));
+            String[] coordinates = prop.getProperty("inputBoundary").split(",");
+            inputBoundary = new Envelope(
+                Double.parseDouble(coordinates[0]),
+                Double.parseDouble(coordinates[1]),
+                Double.parseDouble(coordinates[2]),
+                Double.parseDouble(coordinates[3]));
+            offset = Integer.parseInt(prop.getProperty("offset"));
+            splitter = FileDataSplitter.getFileDataSplitter(prop.getProperty("splitter"));
+            gridType = GridType.getGridType(prop.getProperty("gridType"));
+            indexType = IndexType.getIndexType(prop.getProperty("indexType"));
+            numPartitions = Integer.parseInt(prop.getProperty("numPartitions"));
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } finally {
+            if (input != null) {
+                try {
+                    input.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
