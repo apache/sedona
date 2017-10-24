3cbffdaef44755b1a7c54b484f48170292d8f059
Masha Basmanova <mbasmanova@fb.com>
2017-10-24 14:14:06 -0400

    Fix java.lang.StackOverflowError in StandardQuadTree.insert
    StandardQuadTree.insert could get into an infinite split cycle if there was a lot of data in the corner of the overall extent. This PR puts a cap on the depth of the tree equal to number of desired partitions to avoid the problem.
    
    java.lang.StackOverflowError
      at org.datasyslab.geospark.spatialPartitioning.quadtree.StandardQuadTree.findRegion(StandardQuadTree.java:62)
      at org.datasyslab.geospark.spatialPartitioning.quadtree.StandardQuadTree.insert(StandardQuadTree.java:153)
      at org.datasyslab.geospark.spatialPartitioning.quadtree.StandardQuadTree.insert(StandardQuadTree.java:159)
      at org.datasyslab.geospark.spatialPartitioning.quadtree.StandardQuadTree.insert(StandardQuadTree.java:169)
      at org.datasyslab.geospark.spatialPartitioning.quadtree.StandardQuadTree.insert(StandardQuadTree.java:159)

---
+++ core/src/test/java/org/datasyslab/geospark/spatialPartitioning/quadtree/QuadTreePartitioningTest.java
@@ -0,0 +1,40 @@
+package org.datasyslab.geospark.spatialPartitioning.quadtree;
+
+import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.Envelope;
+import com.vividsolutions.jts.geom.GeometryFactory;
+import com.vividsolutions.jts.geom.Point;
+import org.datasyslab.geospark.spatialPartitioning.QuadtreePartitioning;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class QuadTreePartitioningTest {
+
+    private final GeometryFactory factory = new GeometryFactory();
+
+    /**
+     * Verifies that data skew doesn't cause java.lang.StackOverflowError
+     * in StandardQuadTree.insert
+     */
+    @Test
+    public void testDataSkew() throws Exception {
+
+        // Create an artificially skewed data set of identical envelopes
+        final Point point = factory.createPoint(new Coordinate(0, 0));
+
+        final List<Envelope> samples = new ArrayList<>();
+        for (int i=0; i<1000; i++) {
+            samples.add(point.getEnvelopeInternal());
+        }
+
+        final Envelope extent = new Envelope(0, 1, 0, 1);
+
+        // Make sure Quad-tree is built successfully without throwing
+        // java.lang.StackOverflowError
+        QuadtreePartitioning partitioning = new QuadtreePartitioning(samples, extent, 10);
+        Assert.assertNotNull(partitioning.getPartitionTree());
+    }
+}
