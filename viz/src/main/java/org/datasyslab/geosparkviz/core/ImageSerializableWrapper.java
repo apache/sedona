6efe889b3e09345b2096b92a3746cc144e69f556
Jia Yu <jiayu2@asu.edu>
2017-12-10 01:27:50 -0700

    Replace all bablon with GeoSparkViz

---
+++ viz/src/main/java/org/datasyslab/geosparkviz/core/ImageSerializableWrapper.java
@@ -0,0 +1,71 @@
+/**
+ * FILE: ImageSerializableWrapper.java
+ * PATH: org.datasyslab.geosparkviz.core.ImageSerializableWrapper.java
+ * Copyright (c) 2015-2017 GeoSpark Development Team
+ * All rights reserved.
+ */
+package org.datasyslab.geosparkviz.core;
+
+import java.io.*;
+
+import javax.imageio.ImageIO;
+
+import java.awt.image.*;
+
+// TODO: Auto-generated Javadoc
+/**
+ * The Class ImageSerializableWrapper.
+ */
+public class ImageSerializableWrapper implements Serializable {
+  
+  /** The image. */
+  protected transient BufferedImage image;
+  
+  /**
+   * Instantiates a new image serializable wrapper.
+   *
+   * @param image the image
+   */
+  public ImageSerializableWrapper(BufferedImage image) {
+    this.image = image;
+  }
+
+  /**
+   * Write object.
+   *
+   * @param out the out
+   * @throws IOException Signals that an I/O exception has occurred.
+   */
+  // Serialization method.
+  private void writeObject(ObjectOutputStream out) throws IOException {
+    out.defaultWriteObject();
+    ImageIO.write(image, "png", out);
+  }
+
+  /**
+   * Read object.
+   *
+   * @param in the in
+   * @throws IOException Signals that an I/O exception has occurred.
+   * @throws ClassNotFoundException the class not found exception
+   */
+  // Deserialization method.
+  private void readObject(ObjectInputStream in) throws IOException, ClassNotFoundException {
+    in.defaultReadObject();
+    image = ImageIO.read(in);
+    if(image==null)
+    {
+    	System.out.println("I got nothing from the stream!");
+    }
+  }
+
+  /**
+   * Gets the image.
+   *
+   * @return the image
+   */
+  public BufferedImage getImage()
+  {
+    return this.image;
+  }
+}
---
+++ viz/src/main/java/org/datasyslab/geosparkviz/extension/photoFilter/Sharpen.java
@@ -0,0 +1,33 @@
+/**
+ * FILE: Sharpen.java
+ * PATH: org.datasyslab.geosparkviz.extension.photoFilter.Sharpen.java
+ * Copyright (c) 2015-2017 GeoSpark Development Team
+ * All rights reserved.
+ */
+package org.datasyslab.geosparkviz.extension.photoFilter;
+
+import org.datasyslab.geosparkviz.core.PhotoFilter;
+
+// TODO: Auto-generated Javadoc
+/**
+ * The Class Sharpen.
+ */
+public class Sharpen extends PhotoFilter{
+	
+	/**
+	 * Instantiates a new sharpen.
+	 */
+	public Sharpen() {
+		super(1);
+		this.convolutionMatrix[0][0]=0.0;
+		this.convolutionMatrix[1][0]=-1.0;
+		this.convolutionMatrix[2][0]=0.0;
+		this.convolutionMatrix[0][1]=-1.0;
+		this.convolutionMatrix[1][1]=5.0;
+		this.convolutionMatrix[2][1]=-1.0;
+		this.convolutionMatrix[0][2]=0.0;
+		this.convolutionMatrix[1][2]=-1.0;
+		this.convolutionMatrix[2][2]=0.0;
+	}
+
+}
