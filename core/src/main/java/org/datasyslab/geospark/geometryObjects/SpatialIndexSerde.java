77a61aae80a28bd5ff64d14417dfab1323aa6bd5
zongsizhang <krishnazongsi@gmail.com>
2018-01-03 23:57:18 -0700

    add serializer for strtree

--- core/src/main/java/org/datasyslab/geospark/geometryObjects/SpatialIndexSerde.java
+++ core/src/main/java/org/datasyslab/geospark/geometryObjects/SpatialIndexSerde.java
@@ -11,6 +11,8 @@ import com.vividsolutions.jts.index.SpatialIndex;
 import com.vividsolutions.jts.index.quadtree.Node;
 import com.vividsolutions.jts.index.quadtree.NodeBase;
 import com.vividsolutions.jts.index.quadtree.Quadtree;
+import com.vividsolutions.jts.index.strtree.AbstractNode;
+import com.vividsolutions.jts.index.strtree.ItemBoundable;
 import com.vividsolutions.jts.index.strtree.STRtree;
 import org.apache.log4j.Logger;
 
@@ -61,35 +63,98 @@ public class SpatialIndexSerde extends Serializer{
         if( o instanceof Quadtree){
             // serialize quadtree index
             writeType(output, Type.QUADTREE);
-            writeQuadTree(kryo,output, ((Quadtree) o));
+            Quadtree tree = (Quadtree)o;
+            if(tree.isEmpty()){
+                output.writeByte(0);
+            }else {
+                output.writeByte(1);
+                // write root
+                List items = tree.getRoot().getItems();
+                output.writeInt(items.size());
+                for(Object item : items){
+                    geometrySerde.write(kryo, output, item);
+                }
+                Node[] subNodes= tree.getRoot().getSubnode();
+                for(int i = 0;i < 4; ++i){
+                    writeQuadTreeNode(kryo, output, subNodes[i]);
+                }
+            }
         }else if(o instanceof STRtree){
             //serialize rtree index
             writeType(output, Type.RTREE);
+            STRtree tree = (STRtree)o;
+            output.writeInt(tree.getNodeCapacity());
+            if(tree.isEmpty()){
+                output.writeByte(0);
+            }else{
+                output.writeByte(1);
+                // write head
+                output.writeByte(tree.isBuilt() ? 1 : 0);
+                if(!tree.isBuilt()){
+                    // if not built, itemBoundables will not be null, record it
+                    ArrayList itemBoundables = tree.getItemBoundables();
+                    output.writeInt(itemBoundables.size());
+                    for(Object obj : itemBoundables){
+                        if(!(obj instanceof ItemBoundable)) throw new UnsupportedOperationException(" itemBoundables should only contain ItemBoundable objects ");
+                        ItemBoundable itemBoundable = (ItemBoundable) obj;
+                        // write envelope
+                        writeItemBoundable(kryo, output, itemBoundable);
+                    }
+                }else{
+                    // if built, write from root
+                    writeSTRTreeNode(kryo, output, tree.getRoot());
+                }
+            }
+        } else {
+            throw new UnsupportedOperationException(" index type not supported ");
         }
     }
 
-    /**
-     * write head and root node
-     * byte  1byte  empty or not, 0 means empty, 1 means not empty
-     *
-     * @param kryo
-     * @param output
-     * @param tree
-     */
-    private void writeQuadTree(Kryo kryo, Output output, Quadtree tree){
-        if(tree.isEmpty()){
-            output.writeByte(0);
-        }else {
-            output.writeByte(1);
-            // write root
-            List items = tree.getRoot().getItems();
-            output.writeInt(items.size());
-            for(Object item : items){
-                geometrySerde.write(kryo, output, item);
+    @Override
+    public Object read(Kryo kryo, Input input, Class aClass) {
+        byte typeID = input.readByte();
+        Type indexType = Type.fromId(typeID);
+        switch (indexType){
+            case QUADTREE:{
+                Quadtree index = new Quadtree();
+                boolean notEmpty = (input.readByte() & 0x01) == 1;
+                if(!notEmpty) return index;
+                int itemSize = input.readInt();
+                List items = new ArrayList();
+                for(int i = 0;i < itemSize; ++i){
+                    items.add(geometrySerde.read(kryo, input, Geometry.class));
+                }
+                index.getRoot().setItems(items);
+                for(int i = 0;i < 4; ++i){
+                    index.getRoot().getSubnode()[i] = readQuadTreeNode(kryo, input);
+                }
+                return index;
             }
-            Node[] subNodes= tree.getRoot().getSubnode();
-            for(int i = 0;i < 4; ++i){
-                writeQuadTreeNode(kryo, output, subNodes[i]);
+            case RTREE:{
+                int nodeCapacity = input.readInt();
+                boolean notEmpty = (input.readByte() & 0x01) == 1;
+                if(notEmpty){
+                    STRtree index = new STRtree(nodeCapacity);
+                    boolean built = (input.readByte() & 0x01) == 1;
+                    if(built){
+                        // if built, root is not null, set itemBoundables to null
+                        index.setBuilt(true);
+                        index.setItemBoundables(null);
+                        index.setRoot(readSTRtreeNode(kryo, input));
+                    }else{
+                        // if not built, just read itemBoundables
+                        ArrayList itemBoundables = new ArrayList();
+                        int itemSize = input.readInt();
+                        for(int i = 0;i < itemSize; ++i){
+                            itemBoundables.add(readItemBoundable(kryo, input));
+                        }
+                        index.setItemBoundables(itemBoundables);
+                    }
+                    return index;
+                }else return new STRtree(nodeCapacity);
+            }
+            default:{
+                throw new UnsupportedOperationException("can't deserialize spatial index of type" + indexType);
             }
         }
     }
@@ -115,40 +180,6 @@ public class SpatialIndexSerde extends Serializer{
         }
     }
 
-    private void writeType(Output output, Type type){
-        output.writeByte((byte)type.id);
-    }
-
-    @Override
-    public Object read(Kryo kryo, Input input, Class aClass) {
-        byte typeID = input.readByte();
-        Type indexType = Type.fromId(typeID);
-        switch (indexType){
-            case QUADTREE:{
-                Quadtree index = new Quadtree();
-                boolean notEmpty = (input.readByte() & 0x01) == 1;
-                if(!notEmpty) return index;
-                int itemSize = input.readInt();
-                List items = new ArrayList();
-                for(int i = 0;i < itemSize; ++i){
-                    items.add(geometrySerde.read(kryo, input, Geometry.class));
-                }
-                index.getRoot().setItems(items);
-                for(int i = 0;i < 4; ++i){
-                    index.getRoot().getSubnode()[i] = readQuadTreeNode(kryo, input);
-                }
-                return index;
-            }
-            case RTREE:{
-                break;
-            }
-            default:{
-                throw new UnsupportedOperationException("can't deserialize spatial index of type" + indexType);
-            }
-        }
-        return null;
-    }
-
     private Node readQuadTreeNode(Kryo kryo, Input input){
         boolean notEmpty = (input.readByte() & 0x01) == 1;
         if(!notEmpty) return null;
@@ -166,5 +197,69 @@ public class SpatialIndexSerde extends Serializer{
             node.getSubnode()[i] = readQuadTreeNode(kryo, input);
         }
         return node;
+    }
+
+    private void writeSTRTreeNode(Kryo kryo, Output output, AbstractNode node){
+        // write head
+        output.writeInt(node.getLevel());
+        // write children
+        List children = node.getChildBoundables();
+        int childrenSize = children.size();
+        output.writeInt(childrenSize);
+        // if children not empty, write children
+        if(childrenSize > 0){
+            if(children.get(0) instanceof AbstractNode){
+                // write type as 0, non-leaf node
+                output.writeByte(0);
+                for(Object obj : children){
+                    AbstractNode child = (AbstractNode) obj;
+                    writeSTRTreeNode(kryo, output, child);
+                }
+            }else if(children.get(0) instanceof ItemBoundable){
+                // write type as 1, leaf node
+                output.writeByte(1);
+                // for leaf node, write items
+                for(Object obj : children){
+                    writeItemBoundable(kryo, output, (ItemBoundable)obj);
+                }
+            }else{
+                throw new UnsupportedOperationException("wrong node type of STRtree");
+            }
+        }
+    }
+
+    private STRtree.STRtreeNode readSTRtreeNode(Kryo kryo, Input input){
+        int level = input.readInt();
+        STRtree.STRtreeNode node = new STRtree.STRtreeNode(level);
+        int childrenSize = input.readInt();
+        boolean isLeaf = (input.readByte() & 0x01) == 1;
+        ArrayList children = new ArrayList();
+        if(isLeaf){
+            for(int i = 0;i < childrenSize; ++i){
+                children.add(readItemBoundable(kryo, input));
+            }
+        }else{
+            for(int i = 0;i < childrenSize; ++i){
+                children.add(readSTRtreeNode(kryo, input));
+            }
+        }
+        node.setChildBoundables(children);
+        return node;
+    }
+
+    private void writeItemBoundable(Kryo kryo, Output output, ItemBoundable itemBoundable){
+        geometrySerde.write(kryo, output, itemBoundable.getBounds());
+        geometrySerde.write(kryo, output, itemBoundable.getItem());
+    }
+
+    private ItemBoundable readItemBoundable(Kryo kryo, Input input) {
+        return new ItemBoundable(
+                geometrySerde.read(kryo, input, Envelope.class),
+                geometrySerde.read(kryo, input, Geometry.class)
+        );
+    }
+
+    private void writeType(Output output, Type type){
+        output.writeByte((byte)type.id);
     }
 }
--- core/src/test/java/org/datasyslab/geospark/geometryObjects/SpatialIndexSerdeTest.java
+++ core/src/test/java/org/datasyslab/geospark/geometryObjects/SpatialIndexSerdeTest.java
@@ -4,10 +4,12 @@ import com.esotericsoftware.kryo.Kryo;
 import com.esotericsoftware.kryo.io.Input;
 import com.esotericsoftware.kryo.io.Output;
 import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.Envelope;
 import com.vividsolutions.jts.geom.GeometryFactory;
 import com.vividsolutions.jts.geom.Point;
 import com.vividsolutions.jts.index.SpatialIndex;
 import com.vividsolutions.jts.index.quadtree.Quadtree;
+import com.vividsolutions.jts.index.strtree.STRtree;
 import com.vividsolutions.jts.io.WKTReader;
 import org.junit.Test;
 
@@ -30,32 +32,65 @@ public class SpatialIndexSerdeTest {
 
     @Test
     public void test() throws Exception {
+        //test empty tree
+        testQuadTree(0);
+        testSTRTree(0);
+        // test non-empty tree
+        testQuadTree(10000);
+        testSTRTree(10000);
+    }
+
+    private void testQuadTree(int treeSize) throws Exception {
         SpatialIndexSerde serializer = new SpatialIndexSerde();
         kryo.register(Quadtree.class, serializer);
 
         Output output = new Output(new FileOutputStream("out.dat"));
-        Quadtree tree = generateQuadTree(10000);
-        kryo.writeClassAndObject(output, tree);
+        Quadtree exact = (Quadtree) generateQuadTree(treeSize, Quadtree.class);
+        kryo.writeClassAndObject(output, exact);
         output.close();
 
 
         Input input = new Input(new FileInputStream("out.dat"));
-        Quadtree quadtree = (Quadtree) kryo.readClassAndObject(input);
-        System.out.println(quadtree.queryAll().size());
+        Quadtree expect = (Quadtree) kryo.readClassAndObject(input);
         input.close();
 
-        List sItems = tree.queryAll();
-        List dItems = tree.queryAll();
+        List exactItems = exact.queryAll();
+        List expectItems = expect.queryAll();
 
-        // assert size equals
-        assertEquals(sItems.size(), dItems.size());
         // assert result equals
-        assertThat(sItems, is(dItems));
+        assertThat(exactItems, is(expectItems));
     }
 
-    private Quadtree generateQuadTree(int geomNum){
+    private void testSTRTree(int treeSize) throws Exception {
+        SpatialIndexSerde serializer = new SpatialIndexSerde();
+        kryo.register(STRtree.class, serializer);
+
+        Output output = new Output(new FileOutputStream("out.dat"));
+        STRtree exact = (STRtree) generateQuadTree(treeSize, STRtree.class);
+        kryo.writeClassAndObject(output, exact);
+        output.close();
+
+
+        Input input = new Input(new FileInputStream("out.dat"));
+        STRtree expect = (STRtree) kryo.readClassAndObject(input);
+        input.close();
+
+        // query all points
+        List exactRes = exact.query(new Envelope(-180,180,-90,90));
+        List expectRes = expect.query(new Envelope(-180,180,-90,90));
+
+        // assert size equal and content equal
+        assertThat(exactRes, is(expectRes));
+
+    }
+
+    private SpatialIndex generateQuadTree(int geomNum, Class aClass){
         Random random = new Random();
-        Quadtree quadtree = new Quadtree();
+        SpatialIndex quadtree;
+        // initialize according to class pointed
+        if(aClass == Quadtree.class) quadtree = new Quadtree();
+        else quadtree = new STRtree();
+
         for(int i = 0;i < geomNum; ++i){
             Point point = geometryFactory.createPoint(new Coordinate(
                     random.nextDouble() % 180,
@@ -65,5 +100,7 @@ public class SpatialIndexSerdeTest {
         }
         return quadtree;
     }
+
+
 
 }
