fdbd309ca371402999558f7bf8277e6ccc69c075
devlucasc <developedbylucas@gmail.com>
2018-06-12 19:32:53 -0300

    Added isvalid function

--- sql/src/main/scala/org/apache/spark/sql/geosparksql/expressions/Functions.scala
+++ sql/src/main/scala/org/apache/spark/sql/geosparksql/expressions/Functions.scala
@@ -232,7 +232,7 @@ case class ST_Intersection(inputExpressions: Seq[Expression])
   */
 case class ST_IsValid(inputExpressions: Seq[Expression])
   extends Expression with CodegenFallback {
-  override def nullable: Boolean = true
+  override def nullable: Boolean = false
 
   override def eval(input: InternalRow): Any = {
     assert(inputExpressions.length == 1)
