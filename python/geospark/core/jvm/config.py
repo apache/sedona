755f2da014aedf45fc8b6c190f2825a780b5414e
Kengo Seki <sekikn@apache.org>
2020-08-05 11:15:59 +0900

    Add warning message when 'spark.jars' is not specified
    so that users can trace the problem from the log.

--- python/geospark/core/jvm/config.py
+++ python/geospark/core/jvm/config.py
@@ -65,8 +65,11 @@ class SparkJars:
         java_spark_conf = spark_conf._jconf
         try:
             used_jar_files = java_spark_conf.get("spark.jars")
+        except Exception as e:
+            logging.warning(f"Failed to get the value of spark.jars from SparkConf: {e}")
         finally:
             if not used_jar_files:
+                logging.info("Trying to get filenames from the $SPARK_HOME/jars directory")
                 used_jar_files = ",".join(os.listdir(os.path.join(os.environ["SPARK_HOME"], "jars")))
         return used_jar_files
 
