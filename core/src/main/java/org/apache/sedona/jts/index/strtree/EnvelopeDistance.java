1cf775dc94ef549f4913c0297b848d55e70ad4d9
Jia Yu <jiayu@apache.org>
2020-12-18 00:02:09 -0800

    [SEDONA-3] First release: Set up POM for uploading snapshots and releases (#495)
    * Update GitHub action
    
    * Cache Maven package
    
    * Separate maven and python test
    
    * Update the pom, setup py
    
    * Remove JTS SNAPSHOT dependency and copy 5 JTS files to Sedona
    
    * Update POM
    
    * Update POM
    
    * Update POM
    
    * Update scalastyle
    
    * Update POM
    
    * Update POM
    
    * Change the scope of all dependencies to provided. Put necessary compile dependencies to Python adapter. Rename copied JTS files under org.apache.sedona.jts.index
    
    * Change the scope of all dependencies to provided. Put necessary compile dependencies to Python adapter. Rename copied JTS files under org.apache.sedona.jts.index
    
    * Change the scope of all dependencies to provided. Put necessary compile dependencies to Python adapter. Rename copied JTS files under org.apache.sedona.jts.index
    
    * Change the scope of all dependencies to provided. Put necessary compile dependencies to Python adapter. Rename copied JTS files under org.apache.sedona.jts.index
    
    * Change the scope of all dependencies to provided. Put necessary compile dependencies to Python adapter. Rename copied JTS files under org.apache.sedona.jts.index

---
+++ core/src/main/java/org/apache/sedona/jts/index/strtree/EnvelopeDistance.java
@@ -0,0 +1,120 @@
+/*
+ * Copyright (c) 2019 Martin Davis.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * and Eclipse Distribution License v. 1.0 which accompanies this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v20.html
+ * and the Eclipse Distribution License is available at
+ *
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ */
+package org.apache.sedona.jts.index.strtree;
+
+import org.locationtech.jts.geom.Envelope;
+
+/**
+ * Functions for computing distances between {@link Envelope}s.
+ * 
+ * @author mdavis
+ *
+ */
+public class EnvelopeDistance 
+{
+  /**
+   * Computes the maximum distance between the points defining two envelopes.
+   * It is equal to the length of the diagonal of 
+   * the envelope containing both input envelopes.
+   * This is a coarse upper bound on the distance between 
+   * geometries bounded by the envelopes.
+   * 
+   * @param env1 an envelope
+   * @param env2 an envelope
+   * @return the maximum distance between the points defining the envelopes
+   */
+  public static double maximumDistance(Envelope env1, Envelope env2)
+  {
+    double minx = Math.min(env1.getMinX(), env2.getMinX());
+    double miny = Math.min(env1.getMinY(), env2.getMinY());
+    double maxx = Math.max(env1.getMaxX(), env2.getMaxX());
+    double maxy = Math.max(env1.getMaxY(), env2.getMaxY());
+    return distance(minx, miny, maxx, maxy);
+  }
+  
+  private static double distance(double x1, double y1, double x2, double y2) {
+    double dx = x2 - x1;
+    double dy = y2 - y1;
+    return Math.sqrt(dx * dx + dy * dy);    
+  }
+  
+  /**
+   * Computes the Min-Max Distance between two {@link Envelope}s.
+   * It is equal to the minimum of the maximum distances between all pairs of
+   * edge segments from the two envelopes.
+   * This is the tight upper bound on the distance between 
+   * geometric items bounded by the envelopes.
+   * <p>
+   * Theoretically this bound can be used in the R-tree nearest-neighbour branch-and-bound search
+   * instead of {@link #maximumDistance(Envelope, Envelope)}.
+   * However, little performance improvement is observed in practice.
+   * 
+   * @param a an envelope
+   * @param b an envelope
+   * @return the min-max-distance between the envelopes
+   */
+  public static double minMaxDistance(Envelope a, Envelope b)
+  {
+    double aminx = a.getMinX();
+    double aminy = a.getMinY();
+    double amaxx = a.getMaxX();
+    double amaxy = a.getMaxY();
+    double bminx = b.getMinX();
+    double bminy = b.getMinY();
+    double bmaxx = b.getMaxX();
+    double bmaxy = b.getMaxY();
+    
+    double dist =         maxDistance(aminx, aminy, aminx, amaxy, bminx, bminy, bminx, bmaxy);
+    dist = Math.min(dist, maxDistance(aminx, aminy, aminx, amaxy, bminx, bminy, bmaxx, bminy));
+    dist = Math.min(dist, maxDistance(aminx, aminy, aminx, amaxy, bmaxx, bmaxy, bminx, bmaxy));
+    dist = Math.min(dist, maxDistance(aminx, aminy, aminx, amaxy, bmaxx, bmaxy, bmaxx, bminy));
+  
+    dist = Math.min(dist, maxDistance(aminx, aminy, amaxx, aminy, bminx, bminy, bminx, bmaxy));
+    dist = Math.min(dist, maxDistance(aminx, aminy, amaxx, aminy, bminx, bminy, bmaxx, bminy));
+    dist = Math.min(dist, maxDistance(aminx, aminy, amaxx, aminy, bmaxx, bmaxy, bminx, bmaxy));
+    dist = Math.min(dist, maxDistance(aminx, aminy, amaxx, aminy, bmaxx, bmaxy, bmaxx, bminy));
+    
+    dist = Math.min(dist, maxDistance(amaxx, amaxy, aminx, amaxy, bminx, bminy, bminx, bmaxy));
+    dist = Math.min(dist, maxDistance(amaxx, amaxy, aminx, amaxy, bminx, bminy, bmaxx, bminy));
+    dist = Math.min(dist, maxDistance(amaxx, amaxy, aminx, amaxy, bmaxx, bmaxy, bminx, bmaxy));
+    dist = Math.min(dist, maxDistance(amaxx, amaxy, aminx, amaxy, bmaxx, bmaxy, bmaxx, bminy));
+    
+    dist = Math.min(dist, maxDistance(amaxx, amaxy, amaxx, aminy, bminx, bminy, bminx, bmaxy));
+    dist = Math.min(dist, maxDistance(amaxx, amaxy, amaxx, aminy, bminx, bminy, bmaxx, bminy));
+    dist = Math.min(dist, maxDistance(amaxx, amaxy, amaxx, aminy, bmaxx, bmaxy, bminx, bmaxy));
+    dist = Math.min(dist, maxDistance(amaxx, amaxy, amaxx, aminy, bmaxx, bmaxy, bmaxx, bminy));
+    
+    return dist;
+  }
+
+  /**
+   * Computes the maximum distance between two line segments.
+   * 
+   * @param ax1 x ordinate of first endpoint of segment 1
+   * @param ay1 y ordinate of first endpoint of segment 1
+   * @param ax2 x ordinate of second endpoint of segment 1
+   * @param ay2 y ordinate of second endpoint of segment 1
+   * @param bx1 x ordinate of first endpoint of segment 2
+   * @param by1 y ordinate of first endpoint of segment 2
+   * @param bx2 x ordinate of second endpoint of segment 2
+   * @param by2 y ordinate of second endpoint of segment 2
+   * @return maximum distance between the segments
+   */
+  private static double maxDistance(double ax1, double ay1, double ax2, double ay2, 
+      double bx1, double by1, double bx2, double by2) {
+    double dist = distance(ax1, ay1, bx1, by1);
+    dist = Math.max(dist, distance(ax1, ay1, bx2, by2));
+    dist = Math.max(dist, distance(ax2, ay2, bx1, by1));
+    dist = Math.max(dist, distance(ax2, ay2, bx2, by2));
+    return dist;
+  }
+}
---
+++ core/src/main/java/org/apache/sedona/jts/index/strtree/ItemDistance.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2016 Martin Davis.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * and Eclipse Distribution License v. 1.0 which accompanies this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v20.html
+ * and the Eclipse Distribution License is available at
+ *
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ */
+
+package org.apache.sedona.jts.index.strtree;
+
+
+/**
+ * A function method which computes the distance
+ * between two {@link ItemBoundable}s in an {@link STRtree}.
+ * Used for Nearest Neighbour searches.
+ * <p>
+ * To make a distance function suitable for
+ * querying a single index tree
+ * via {@link STRtree#nearestNeighbour(ItemDistance)} ,
+ * the function should have a non-zero <i>reflexive distance</i>.
+ * That is, if the two arguments are the same object,
+ * the distance returned should be non-zero.
+ * If it is required that only pairs of <b>distinct</b> items be returned,
+ * the distance function must be <i>anti-reflexive</i>,
+ * and must return {@link Double#MAX_VALUE} for identical arguments.
+ * 
+ * @author Martin Davis
+ *
+ */
+public interface ItemDistance 
+{
+  /**
+   * Computes the distance between two items.
+   * 
+   * @param item1
+   * @param item2
+   * @return the distance between the items
+   * 
+   * @throws IllegalArgumentException if the metric is not applicable to the arguments
+   */
+  double distance(ItemBoundable item1, ItemBoundable item2);
+
+}
