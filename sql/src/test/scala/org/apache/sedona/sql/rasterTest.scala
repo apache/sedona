28f32624049faf72064bdfcf1e30d08149a83055
Jia Yu <jiayu198910@gmail.com>
2021-05-29 12:10:02 -0700

    Improve the test

--- sql/src/test/scala/org/apache/sedona/sql/rasterTest.scala
+++ sql/src/test/scala/org/apache/sedona/sql/rasterTest.scala
@@ -19,6 +19,7 @@
 
 package org.apache.sedona.sql
 
+import org.locationtech.jts.geom.Geometry
 import org.scalatest.{BeforeAndAfter, GivenWhenThen}
 
 import scala.collection.mutable
@@ -30,17 +31,24 @@ class rasterTest extends TestBaseScala with BeforeAndAfter with GivenWhenThen {
 
     it("Should Pass geotiff loading") {
       var df = sparkSession.read.format("geotiff").option("dropInvalid", true).load(rasterdatalocation)
-      df.printSchema()
       df = df.selectExpr("image.origin as origin","image.Geometry as Geom", "image.height as height", "image.width as width", "image.data as data", "image.nBands as bands")
-      assert(df.count()==2)
-
+      assert(df.first().getAs[Geometry](1).toText == "POLYGON ((-117.64141128097314 33.94356351407699, -117.64141128097314 33.664978146501284, -117.30939395196258 33.664978146501284," +
+        " -117.30939395196258 33.94356351407699, -117.64141128097314 33.94356351407699))")
+      assert(df.first().getInt(2) == 517)
+      assert(df.first().getInt(3) == 512)
+      assert(df.first().getInt(5) == 1)
+      val blackBand = df.first().getAs[mutable.WrappedArray[Double]](4)
+      val line1 = blackBand.slice(0, 512)
+      val line2 = blackBand.slice(512, 1024)
+      assert(line1(0) == 0.0) // The first value at line 1 is black
+      assert(line2(159) == 0.0 && line2(160) == 123.0) // In the second line, value at 159 is black and at 160 is not black
     }
 
     it("should pass RS_GetBand") {
       var df = sparkSession.read.format("geotiff").option("dropInvalid", true).load(resourceFolder + "raster/")
       df = df.selectExpr(" image.data as data", "image.nBands as bands")
       df = df.selectExpr("RS_GetBand(data, 1, bands) as targetBand")
-      assert(df.first().getAs[mutable.WrappedArray[Double]](0).length == 32*32)
+      assert(df.first().getAs[mutable.WrappedArray[Double]](0).length == 512 * 517)
     }
 
   it("should pass RS_Base64") {
@@ -48,7 +56,7 @@ class rasterTest extends TestBaseScala with BeforeAndAfter with GivenWhenThen {
     df = df.selectExpr(" image.data as data", "image.nBands as bands")
     df = df.selectExpr("RS_GetBand(data, 1, bands) as targetBand")
     df = df.selectExpr("RS_Base64(targetBand) as baseString")
-    df.show()
+    println(df.first().getString(0))
   }
 
 }
