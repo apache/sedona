f13282ac623c04fce03dcb4c26e4d1ff0d93c87d
madzik555 <wojciak.magdalena31@gmail.com>
2022-03-13 15:48:38 +0100

    Fix spaces

--- python/tests/sql/test_function.py
+++ python/tests/sql/test_function.py
@@ -318,7 +318,7 @@ class TestPredicateJoin(TestBase):
         diff = self.spark.sql("select ST_SymDifference(a,b) from test_sym_diff")
         assert diff.take(1)[0][0].wkt == "POLYGON ((-3 -3, -3 3, 3 3, 3 -3, -3 -3), (-1 -1, 1 -1, 1 1, -1 1, -1 -1))"
 
-	def test_st_union_part_of_right_overlaps_left(self):
+    def test_st_union_part_of_right_overlaps_left(self):
         test_table = self.spark.sql("select ST_GeomFromWKT('POLYGON ((-3 -3, 3 -3, 3 3, -3 3, -3 -3))') as a, ST_GeomFromWKT('POLYGON ((-2 1, 2 1, 2 4, -2 4, -2 1))') as b")
         test_table.createOrReplaceTempView("test_union")
         union = self.spark.sql("select ST_Union(a,b) from test_union")
