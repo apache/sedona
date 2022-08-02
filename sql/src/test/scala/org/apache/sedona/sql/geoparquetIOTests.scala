d7dce2a5a4d140c451c7ad6847e1e66848e40486
Jia Yu <jiayu198910@gmail.com>
2022-08-02 11:52:27 -0700

    Update the test

--- sql/src/test/scala/org/apache/sedona/sql/geoparquetIOTests.scala
+++ sql/src/test/scala/org/apache/sedona/sql/geoparquetIOTests.scala
@@ -1,5 +1,6 @@
 package org.apache.sedona.sql
 
+import org.apache.spark.sql.SaveMode
 import org.locationtech.jts.geom.Geometry
 import org.scalatest.BeforeAndAfter
 
@@ -19,7 +20,7 @@ class geoparquetIOTests extends TestBaseScala  with BeforeAndAfter{
       assert(rows.getAs[String]("iso_a3") == "FJI")
       assert(rows.getAs[Double]("gdp_md_est") == 8374.0)
       assert(rows.getAs[Geometry]("geometry").toString == "MULTIPOLYGON (((180 -16.067132663642447, 180 -16.555216566639196, 179.36414266196414 -16.801354076946883, 178.72505936299711 -17.01204167436804, 178.59683859511713 -16.639150000000004, 179.0966093629971 -16.433984277547403, 179.4135093629971 -16.379054277547404, 180 -16.067132663642447)), ((178.12557 -17.50481, 178.3736 -17.33992, 178.71806 -17.62846, 178.55271 -18.15059, 177.93266000000003 -18.28799, 177.38146 -18.16432, 177.28504 -17.72465, 177.67087 -17.381140000000002, 178.12557 -17.50481)), ((-179.79332010904864 -16.020882256741224, -179.9173693847653 -16.501783135649397, -180 -16.555216566639196, -180 -16.067132663642447, -179.79332010904864 -16.020882256741224)))")
-      df.write.format("geoparquet").save(geoparquetoutputlocation + "/gp_sample1.parquet")
+      df.write.format("geoparquet").mode(SaveMode.Overwrite).save(geoparquetoutputlocation + "/gp_sample1.parquet")
       val df2 = sparkSession.read.format("geoparquet").load(geoparquetoutputlocation + "/gp_sample1.parquet")
       val newrows = df2.collect()(0)
       assert(newrows.getAs[Geometry]("geometry").toString == "MULTIPOLYGON (((180 -16.067132663642447, 180 -16.555216566639196, 179.36414266196414 -16.801354076946883, 178.72505936299711 -17.01204167436804, 178.59683859511713 -16.639150000000004, 179.0966093629971 -16.433984277547403, 179.4135093629971 -16.379054277547404, 180 -16.067132663642447)), ((178.12557 -17.50481, 178.3736 -17.33992, 178.71806 -17.62846, 178.55271 -18.15059, 177.93266000000003 -18.28799, 177.38146 -18.16432, 177.28504 -17.72465, 177.67087 -17.381140000000002, 178.12557 -17.50481)), ((-179.79332010904864 -16.020882256741224, -179.9173693847653 -16.501783135649397, -180 -16.555216566639196, -180 -16.067132663642447, -179.79332010904864 -16.020882256741224)))")
@@ -29,7 +30,7 @@ class geoparquetIOTests extends TestBaseScala  with BeforeAndAfter{
       val rows = df.collect()(0)
       assert(rows.getAs[String]("name") == "Vatican City")
       assert(rows.getAs[Geometry]("geometry").toString == "POINT (12.453386544971766 41.903282179960115)")
-      df.write.format("geoparquet").save(geoparquetoutputlocation + "/gp_sample2.parquet")
+      df.write.format("geoparquet").mode(SaveMode.Overwrite).save(geoparquetoutputlocation + "/gp_sample2.parquet")
       val df2 = sparkSession.read.format("geoparquet").load(geoparquetoutputlocation + "/gp_sample2.parquet")
       val newrows = df2.collect()(0)
       assert(newrows.getAs[String]("name") == "Vatican City")
@@ -43,7 +44,7 @@ class geoparquetIOTests extends TestBaseScala  with BeforeAndAfter{
       assert(rows.getAs[Double]("Shape_Leng") == 330470.010332)
       assert(rows.getAs[Double]("Shape_Area") == 1.62381982381E9)
       assert(rows.getAs[Geometry]("geometry").toString.startsWith("MULTIPOLYGON (((970217.022"))
-      df.write.format("geoparquet").save(geoparquetoutputlocation + "/gp_sample3.parquet")
+      df.write.format("geoparquet").mode(SaveMode.Overwrite).save(geoparquetoutputlocation + "/gp_sample3.parquet")
       val df2 = sparkSession.read.format("geoparquet").load(geoparquetoutputlocation + "/gp_sample3.parquet")
       val newrows = df2.collect()(0)
       assert(newrows.getAs[Geometry]("geometry").toString.startsWith("MULTIPOLYGON (((970217.022"))
@@ -56,7 +57,7 @@ class geoparquetIOTests extends TestBaseScala  with BeforeAndAfter{
       assert(rows.getAs[Double]("Shape_Leng") == 330470.010332)
       assert(rows.getAs[Double]("Shape_Area") == 1.62381982381E9)
       assert(rows.getAs[Geometry]("geometry").toString.startsWith("MULTIPOLYGON (((970217.022"))
-      df.write.format("geoparquet").save(geoparquetoutputlocation + "/gp_sample3o.parquet")
+      df.write.format("geoparquet").mode(SaveMode.Overwrite).save(geoparquetoutputlocation + "/gp_sample3o.parquet")
       val df2 = sparkSession.read.format("geoparquet").load(geoparquetoutputlocation + "/gp_sample3.parquet")
       val newrows = df2.collect()(0)
       assert(newrows.getAs[Geometry]("geometry").toString.startsWith("MULTIPOLYGON (((970217.022"))
