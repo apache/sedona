eb7d1c2822d2afc2eeaf1e00bbf32fc39a42625d
Doug Dennis <douglasdennisjr@gmail.com>
2022-09-21 10:08:22 -0700

    created and implemented st_predicates.scala

---
+++ sql/src/main/scala/org/apache/spark/sql/sedona_sql/expressions/st_predicates.scala
@@ -0,0 +1,42 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.spark.sql.sedona_sql.expressions
+
+import org.apache.spark.sql.catalyst.expressions.Expression
+import org.apache.spark.sql.Column
+
+object st_predicates extends DataFrameAPI {
+  def ST_Contains(a: ColumnOrName, b: ColumnOrName): Column = wrapExpression[ST_Contains](a, b)
+
+  def ST_Crosses(a: ColumnOrName, b: ColumnOrName): Column = wrapExpression[ST_Crosses](a, b)
+
+  def ST_Disjoint(a: ColumnOrName, b: ColumnOrName): Column = wrapExpression[ST_Disjoint](a, b)
+
+  def ST_Equals(a: ColumnOrName, b: ColumnOrName): Column = wrapExpression[ST_Equals](a, b)
+
+  def ST_Intersects(a: ColumnOrName, b: ColumnOrName): Column = wrapExpression[ST_Intersects](a, b)
+
+  def ST_OrderingEquals(a: ColumnOrName, b: ColumnOrName): Column = wrapExpression[ST_OrderingEquals](a, b)
+
+  def ST_Overlaps(a: ColumnOrName, b: ColumnOrName): Column = wrapExpression[ST_Overlaps](a, b)
+
+  def ST_Touches(a: ColumnOrName, b: ColumnOrName): Column = wrapExpression[ST_Touches](a, b)
+
+  def ST_Within(a: ColumnOrName, b: ColumnOrName): Column = wrapExpression[ST_Within](a, b)
+}
