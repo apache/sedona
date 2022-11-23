ab4d8266fe9b48b451b13c40d0a44747936db311
Varsha Ravindra <varsharavindra12@gmail.com>
2022-11-22 18:00:18 -0700

    [SEDONA-197] Added ST_ZMax and ST_ZMin to Apache Sedona (#717)

--- flink/src/main/java/org/apache/sedona/flink/expressions/Functions.java
+++ flink/src/main/java/org/apache/sedona/flink/expressions/Functions.java
@@ -107,6 +107,22 @@ public class Functions {
         }
     }
 
+    public static class ST_ZMax extends ScalarFunction {
+        @DataTypeHint("Double")
+        public Double eval(@DataTypeHint(value = "RAW", bridgedTo = org.locationtech.jts.geom.Geometry.class) Object o){
+            Geometry geom = (Geometry) o;
+            return org.apache.sedona.common.Functions.zMax(geom);
+        }
+    }
+
+    public static class ST_ZMin extends ScalarFunction {
+        @DataTypeHint("Double")
+        public Double eval(@DataTypeHint(value = "RAW", bridgedTo = org.locationtech.jts.geom.Geometry.class) Object o){
+            Geometry geom = (Geometry) o;
+            return org.apache.sedona.common.Functions.zMin(geom);
+        }
+    }
+
 
     public static class ST_Transform extends ScalarFunction {
         @DataTypeHint(value = "RAW", bridgedTo = org.locationtech.jts.geom.Geometry.class)
