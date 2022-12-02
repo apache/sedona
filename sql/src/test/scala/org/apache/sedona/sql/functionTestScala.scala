127e6e82719cb17c024d2980f6c06bb4d4e21adc
Amrit-Bhaskar-abhask10 <abhask10@asu.edu>
2022-12-02 14:21:05 -0700

    Fixed functionTestScala

--- sql/src/test/scala/org/apache/sedona/sql/functionTestScala.scala
+++ sql/src/test/scala/org/apache/sedona/sql/functionTestScala.scala
@@ -471,7 +471,7 @@ class functionTestScala extends TestBaseScala with Matchers with GeometrySample 
 
     it("Passed ST_NDims with M coordinates") {
       val test = sparkSession.sql("SELECT ST_NDims(ST_GeomFromWKT('POINTM(1 1 0.5)'))")
-      assert(test.take(1)(0).get(0).asInstanceOf[Int] == 3)
+      assert(test.take(1)(0).get(0).asInstanceOf[Int] == 2)
     }
 
     it("Passed ST_NDims with Z coordinates") {
@@ -481,7 +481,7 @@ class functionTestScala extends TestBaseScala with Matchers with GeometrySample 
 
     it("Passed ST_NDims with Z and M coordinates") {
       val test = sparkSession.sql("SELECT ST_NDims(ST_GeomFromWKT('POINTZM(1 1 1 0.5)'))")
-      assert(test.take(1)(0).get(0).asInstanceOf[Int] == 4)
+      assert(test.take(1)(0).get(0).asInstanceOf[Int] == 3)
     }
 
     it("Passed ST_GeometryType") {
