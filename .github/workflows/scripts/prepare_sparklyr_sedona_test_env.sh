b39edb4ab44b46f2f75e4526134b014d4522ff57
gregleleu <33321678+gregleleu@users.noreply.github.com>
2023-03-04 01:40:44 -0500

    Update prepare_sparklyr_sedona_test_env.sh

--- .github/workflows/scripts/prepare_sparklyr_sedona_test_env.sh
+++ .github/workflows/scripts/prepare_sparklyr_sedona_test_env.sh
@@ -20,23 +20,7 @@
 sedona_jar_files () {
   local subdir
   for subdir in 'spark-shaded' 'viz'; do
-    local artifact_id="$(
-      mvn \
-        org.apache.maven.plugins:maven-help-plugin:3.2.0:evaluate \
-        -Dexpression=project.artifactId \
-        -q \
-        -DforceStdout \
-        -f "${subdir}"/target/resolved-pom.xml
-    )"
-    local artifact_version="$(
-      mvn \
-        org.apache.maven.plugins:maven-help-plugin:3.2.0:evaluate \
-        -Dexpression=project.version \
-        -q \
-        -DforceStdout \
-        -f "${subdir}"/target/resolved-pom.xml
-    )"
-    echo "$(pwd)/${subdir}/target/${artifact_id}-${artifact_version}.jar"
+    echo "$(find ${subdir}/target -name 'sedona-*.jar' \! -name '*javadoc*')"
   done
 }
 
