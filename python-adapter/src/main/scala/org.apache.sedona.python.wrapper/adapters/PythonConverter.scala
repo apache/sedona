8b5037ca76aaff95988fc9035cf220a5e70087e0
Doug Dennis <douglasdennisjr@gmail.com>
2023-03-05 18:24:38 -0800

    fix long import line

--- python-adapter/src/main/scala/org.apache.sedona.python.wrapper/adapters/PythonConverter.scala
+++ python-adapter/src/main/scala/org.apache.sedona.python.wrapper/adapters/PythonConverter.scala
@@ -19,7 +19,13 @@
 
 package org.apache.sedona.python.wrapper.adapters
 
-import org.apache.sedona.python.wrapper.translation.{FlatPairRddConverter, GeometryRddConverter, GeometrySeqToPythonConverter, ListPairRddConverter, PythonRDDToJavaConverter}
+import org.apache.sedona.python.wrapper.translation.{
+  FlatPairRddConverter,
+  GeometryRddConverter,
+  GeometrySeqToPythonConverter,
+  ListPairRddConverter,
+  PythonRDDToJavaConverter
+}
 import org.apache.spark.api.java.{JavaPairRDD, JavaRDD}
 import org.locationtech.jts.geom.Geometry
 
