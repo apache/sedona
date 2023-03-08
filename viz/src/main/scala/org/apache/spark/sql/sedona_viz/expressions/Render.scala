033f3424c003516b62e4ea2624bb9ab883817dc1
Doug Dennis <douglasdennisjr@gmail.com>
2023-03-07 17:13:33 -0800

    fix wildcard import in Render

--- viz/src/main/scala/org/apache/spark/sql/sedona_viz/expressions/Render.scala
+++ viz/src/main/scala/org/apache/spark/sql/sedona_viz/expressions/Render.scala
@@ -26,7 +26,7 @@ import org.apache.spark.internal.Logging
 import org.apache.spark.sql.Row
 import org.apache.spark.sql.expressions.{MutableAggregationBuffer, UserDefinedAggregateFunction}
 import org.apache.spark.sql.sedona_viz.UDT.{ImageWrapperUDT, PixelUDT}
-import org.apache.spark.sql.types._
+import org.apache.spark.sql.types.{ArrayType, DataType, IntegerType, StructType}
 
 case class ST_Render() extends UserDefinedAggregateFunction with Logging {
   // This is the input fields for your aggregate function.
