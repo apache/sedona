a76104d3ca02089929b7d7a3b8626c3dceea2b0e
yyy1000 <992364620@qq.com>
2023-07-25 21:17:53 +0800

    add bash script to accelarate

---
+++ docker.sh
@@ -0,0 +1,4 @@
+docker build -t kartikeyhadiya/spark-base:3.3.2 -f docker/spark-base.dockerfile  .
+docker build -t kartikeyhadiya/spark-master -f docker/spark-master.dockerfile .
+docker build -t kartikeyhadiya/spark-worker -f docker/spark-worker.dockerfile .
+docker build -t kartikeyhadiya/sedona_jupyterlab -f docker/spark-sedona-jupyterlab/sedona_jupyterlab.dockerfile .
