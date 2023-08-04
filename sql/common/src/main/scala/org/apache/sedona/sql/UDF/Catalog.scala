41ec834925cabecff20edb647ad641331bebf88f
yyy1000 <992364620@qq.com>
2023-08-04 10:18:11 +0800

    feat: port to Spark

--- sql/common/src/main/scala/org/apache/sedona/sql/UDF/Catalog.scala
+++ sql/common/src/main/scala/org/apache/sedona/sql/UDF/Catalog.scala
@@ -124,6 +124,7 @@ object Catalog {
     function[ST_SubDivideExplode](),
     function[ST_SubDivide](),
     function[ST_MakeLine](),
+    function[ST_Polygon](),
     function[ST_MakePolygon](null),
     function[ST_GeoHash](),
     function[ST_GeomFromGeoHash](null),
