113daf538a8a40c74e2f2168efd9e0863227f146
Nilesh Gajwani <nileshgajwani@gmail.com>
2023-08-16 15:10:46 -0400

    Refactor geomutils

--- common/src/main/java/org/apache/sedona/common/utils/GeomUtilsGeoTools.java
+++ common/src/main/java/org/apache/sedona/common/utils/GeomUtilsGeoTools.java
@@ -1,2 +1,15 @@
-package org.apache.sedona.common.utils;public class GeomUtilsGeoTools {
+package org.apache.sedona.common.utils;
+import org.geotools.geometry.jts.JTS;
+import org.geotools.referencing.CRS;
+import org.locationtech.jts.geom.Geometry;
+import org.opengis.referencing.FactoryException;
+import org.opengis.referencing.crs.CoordinateReferenceSystem;
+import org.opengis.referencing.operation.MathTransform;
+import org.opengis.referencing.operation.TransformException;
+
+public class GeomUtilsGeoTools {
+    public static Geometry transform(Geometry geometry, CoordinateReferenceSystem sourceCRS, CoordinateReferenceSystem targetCRS, boolean lenient) throws FactoryException, TransformException {
+        MathTransform transform = CRS.findMathTransform(sourceCRS, targetCRS, lenient);
+        return JTS.transform(geometry, transform);
+    }
 }
