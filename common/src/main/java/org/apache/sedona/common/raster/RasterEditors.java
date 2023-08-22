7503b6c193554e5850226fc9b990cede831c823e
Furqaanahmed Khan <furqaankhan98@gmail.com>
2023-08-21 22:53:49 -0400

    feat: add RS_SetGeoReference implementation

--- common/src/main/java/org/apache/sedona/common/raster/RasterEditors.java
+++ common/src/main/java/org/apache/sedona/common/raster/RasterEditors.java
@@ -18,12 +18,17 @@
  */
 package org.apache.sedona.common.raster;
 
+import org.apache.sedona.common.utils.RasterUtils;
 import org.geotools.coverage.CoverageFactoryFinder;
 import org.geotools.coverage.grid.GridCoverage2D;
 import org.geotools.coverage.grid.GridCoverageFactory;
+import org.geotools.coverage.grid.GridEnvelope2D;
+import org.geotools.coverage.grid.GridGeometry2D;
 import org.geotools.referencing.CRS;
 import org.geotools.referencing.crs.DefaultEngineeringCRS;
+import org.geotools.referencing.operation.transform.AffineTransform2D;
 import org.opengis.coverage.grid.GridCoverage;
+import org.opengis.metadata.spatial.PixelOrientation;
 import org.opengis.referencing.FactoryException;
 import org.opengis.referencing.crs.CoordinateReferenceSystem;
 import org.opengis.referencing.operation.MathTransform2D;
@@ -46,5 +51,51 @@ public class RasterEditors
         Map<?, ?> properties = raster.getProperties();
         GridCoverage[] sources = raster.getSources().toArray(new GridCoverage[0]);
         return gridCoverageFactory.create(raster.getName().toString(), raster.getRenderedImage(), crs, transform, raster.getSampleDimensions(), sources, properties);
+    }
+
+    public static GridCoverage2D setGeoReference(GridCoverage2D raster, String geoRefCoords, String format) {
+        String[] coords = geoRefCoords.split(" ");
+        if (coords.length != 6) {
+            return null;
+        }
+
+        double scaleX = Double.parseDouble(coords[0]);
+        double skewY = Double.parseDouble(coords[1]);
+        double skewX = Double.parseDouble(coords[2]);
+        double scaleY = Double.parseDouble(coords[3]);
+        double upperLeftX = Double.parseDouble(coords[4]);
+        double upperLeftY = Double.parseDouble(coords[5]);
+        AffineTransform2D affine;
+
+
+        if (format.equalsIgnoreCase("GDAL")) {
+            affine = new AffineTransform2D(scaleX, skewY, skewX, scaleY, upperLeftX, upperLeftY);
+        } else if (format.equalsIgnoreCase("ESRI")) {
+            upperLeftX = upperLeftX - (scaleX * 0.5);
+            upperLeftY = upperLeftY - (scaleY * 0.5);
+            affine = new AffineTransform2D(scaleX, skewY, skewX, scaleY, upperLeftX, upperLeftY);
+        } else {
+            throw new IllegalArgumentException("Please select between the following formats GDAL and ESRI");
+        }
+        int height = RasterAccessors.getHeight(raster), width = RasterAccessors.getWidth(raster);
+
+        GridGeometry2D gridGeometry2D = new GridGeometry2D(
+                new GridEnvelope2D(0, 0, width, height),
+                PixelOrientation.UPPER_LEFT,
+                affine, raster.getCoordinateReferenceSystem(), null
+        );
+
+        return RasterUtils.create(raster.getRenderedImage(), gridGeometry2D, raster.getSampleDimensions(),
+                RasterUtils.getNoDataValue(raster.getSampleDimension(0)));
+    }
+
+    public static GridCoverage2D setGeoReference(GridCoverage2D raster, String geoRefCoords) {
+        return setGeoReference(raster, geoRefCoords, "GDAL");
+    }
+
+    public static GridCoverage2D setGeoReference(GridCoverage2D raster, double upperLeftX, double upperLeftY,
+                                                 double scaleX, double scaleY, double skewX, double skewY) {
+        String geoRedCoord = String.format("%f %f %f %f %f %f", scaleX, skewY, skewX, scaleY, upperLeftX, upperLeftY);
+        return setGeoReference(raster, geoRedCoord, "GDAL");
     }
 }
