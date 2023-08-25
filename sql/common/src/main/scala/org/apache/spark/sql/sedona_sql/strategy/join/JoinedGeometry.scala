75af01c20c7a562f4f6773b7f9f51be0742d1252
Kristin Cowalcijk <bo@wherobots.com>
2023-08-25 18:30:15 +0800

    Reimplement envelope expansion algorithm for geographic distance to take pole and anti-meridian into consideration

--- sql/common/src/main/scala/org/apache/spark/sql/sedona_sql/strategy/join/JoinedGeometry.scala
+++ sql/common/src/main/scala/org/apache/spark/sql/sedona_sql/strategy/join/JoinedGeometry.scala
@@ -33,33 +33,46 @@ object JoinedGeometry {
    */
   def geometryToExpandedEnvelope(geom: Geometry, distance: Double, isGeography: Boolean): Geometry = {
     val envelope = geom.getEnvelopeInternal.copy()
-    // Here we use the polar radius of the spheroid as the radius of the sphere, so that the expanded
-    // envelope will work for both spherical and spheroidal distances.
-    expandEnvelope(envelope, distance, 6357000.0, isGeography)
-    geom.getFactory.toGeometry(envelope)
+    val expandedEnvelope = if (isGeography) {
+      // Here we use the polar radius of the spheroid as the radius of the sphere, so that the expanded
+      // envelope will work for both spherical and spheroidal distances.
+      expandEnvelopeForGeography(envelope, distance, 6357000.0)
+    } else {
+      val newEnvelope = envelope.copy()
+      newEnvelope.expandBy(distance)
+      newEnvelope
+    }
+    geom.getFactory.toGeometry(expandedEnvelope)
   }
 
   /**
    * Expand the given envelope by the given distance in meter.
-   * For geography, we expand the envelope by the given distance in both longitude and latitude.
    *
-   * @param envelope the envelope to expand
-   * @param distance in meter
+   * @param envelope     the envelope to expand
+   * @param distance     in meter
    * @param sphereRadius in meter
-   * @param isGeography whether the envelope is on a sphere
    */
-  private def expandEnvelope(envelope: Envelope, distance: Double, sphereRadius: Double, isGeography: Boolean): Unit = {
-    if (isGeography) {
-      val scaleFactor = 1.1 // 10% buffer to get rid of false negatives
-      val latRadian = Math.toRadians((envelope.getMinY + envelope.getMaxY) / 2.0)
-      val latDeltaRadian = distance / sphereRadius;
-      val latDeltaDegree = Math.toDegrees(latDeltaRadian)
-      val lonDeltaRadian = Math.max(Math.abs(distance / (sphereRadius * Math.cos(latRadian + latDeltaRadian))),
-        Math.abs(distance / (sphereRadius * Math.cos(latRadian - latDeltaRadian))))
-      val lonDeltaDegree = Math.toDegrees(lonDeltaRadian)
-      envelope.expandBy(latDeltaDegree * scaleFactor, lonDeltaDegree * scaleFactor)
+  def expandEnvelopeForGeography(envelope: Envelope, distance: Double, sphereRadius: Double): Envelope = {
+    val scaleFactor = 1.1 // 10% buffer to get rid of false negatives
+    val latDeltaRadian = distance / sphereRadius
+    val latDeltaDegree = Math.toDegrees(latDeltaRadian)
+    val newMinY = envelope.getMinY - latDeltaDegree * scaleFactor
+    val newMaxY = envelope.getMaxY + latDeltaDegree * scaleFactor
+    if (newMinY <= -90 || newMaxY >= 90) {
+      new Envelope(-180, 180, Math.max(newMinY, -90), Math.min(newMaxY, 90))
     } else {
-      envelope.expandBy(distance)
+      val minLatRadian = Math.toRadians(newMinY)
+      val maxLatRadian = Math.toRadians(newMaxY)
+      val lonDeltaRadian = Math.max(Math.abs(distance / (sphereRadius * Math.cos(maxLatRadian))),
+        Math.abs(distance / (sphereRadius * Math.cos(minLatRadian))))
+      val lonDeltaDegree = Math.toDegrees(lonDeltaRadian)
+      val newMinX = envelope.getMinX - lonDeltaDegree * scaleFactor
+      val newMaxX = envelope.getMaxX + lonDeltaDegree * scaleFactor
+      if (newMinX <= -180 || newMaxX >= 180) {
+        new Envelope(-180, 180, newMinY, newMaxY)
+      } else {
+        new Envelope(newMinX, newMaxX, newMinY, newMaxY)
+      }
     }
   }
 }
