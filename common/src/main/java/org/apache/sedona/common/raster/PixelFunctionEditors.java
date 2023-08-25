0cd9d1b128caed536400397cf451025970919eeb
Furqaanahmed Khan <furqaankhan98@gmail.com>
2023-08-25 11:42:36 -0400

    init: initial commit

---
+++ common/src/main/java/org/apache/sedona/common/raster/PixelFunctionEditors.java
@@ -0,0 +1,71 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.sedona.common.raster;
+
+import org.apache.sedona.common.utils.RasterUtils;
+import org.geotools.coverage.GridSampleDimension;
+import org.geotools.coverage.grid.GridCoverage2D;
+
+import javax.media.jai.RasterFactory;
+import java.awt.*;
+import java.awt.image.Raster;
+import java.awt.image.RenderedImage;
+import java.awt.image.WritableRaster;
+
+public class PixelFunctionEditors {
+
+    public static GridCoverage2D setValues(GridCoverage2D raster, int band, int colX, int rowY, int width, int height, double value, boolean keepNoData) {
+        ensureBand(raster, band);
+        RenderedImage originalImage = raster.getRenderedImage();
+        Raster rasterTemp = originalImage.getData();
+        Point location = raster.getRenderedImage().getData().getBounds().getLocation();
+        WritableRaster wr = RasterFactory.createBandedRaster(rasterTemp.getDataBuffer().getDataType(), originalImage.getWidth(), originalImage.getHeight(), raster.getNumSampleDimensions(), location);
+
+        WritableRaster rasterCopied = raster.getRenderedImage().copyData(wr);
+
+        Double noDataValue = null;
+        if (keepNoData) {
+            noDataValue = RasterBandAccessors.getBandNoDataValue(raster, band);
+        }
+
+        for (int i = colX; i < colX + width; i++) {
+            for (int j = rowY; j < rowY + height; j++) {
+                double[] pixel = rasterCopied.getPixel(i, j, (double[]) null);
+                if (keepNoData && noDataValue != pixel[band - 1]) {
+                    pixel[band - 1] = value;
+                } else if (!keepNoData) {
+                    pixel[band - 1] = value;
+                }
+                rasterCopied.setPixel(i, j, pixel);
+            }
+        }
+        return RasterUtils.create(wr, raster.getGridGeometry(), raster.getSampleDimensions());
+    }
+
+    public static GridCoverage2D setValues(GridCoverage2D raster, int band, int colX, int rowY, int width, int height, double value) {
+        return setValues(raster, band, colX, rowY, width, height, value, false);
+    }
+
+    private static void ensureBand(GridCoverage2D raster, int band) throws IllegalArgumentException {
+        if (band > RasterAccessors.numBands(raster)) {
+            throw new IllegalArgumentException(String.format("Provided band index %d is not present in the raster", band));
+        }
+    }
+
+}
