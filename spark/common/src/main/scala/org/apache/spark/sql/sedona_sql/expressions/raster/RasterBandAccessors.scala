ecc25515b0b4fa47aa10fa4e828e98eb853c9b01
Jia Yu <jiayu@apache.org>
2023-09-06 22:35:43 -0700

    Fetch upstream (#1007)
    Co-authored-by: Furqaanahmed Khan <46216254+furqaankhan@users.noreply.github.com>

--- spark/common/src/main/scala/org/apache/spark/sql/sedona_sql/expressions/raster/RasterBandAccessors.scala
+++ spark/common/src/main/scala/org/apache/spark/sql/sedona_sql/expressions/raster/RasterBandAccessors.scala
@@ -20,9 +20,14 @@
 package org.apache.spark.sql.sedona_sql.expressions.raster
 
 import org.apache.sedona.common.raster.RasterBandAccessors
+import org.apache.spark.sql.catalyst.InternalRow
 import org.apache.spark.sql.catalyst.expressions.Expression
+import org.apache.spark.sql.catalyst.util.ArrayData
+import org.apache.spark.sql.sedona_sql.UDT.RasterUDT
 import org.apache.spark.sql.sedona_sql.expressions.InferrableFunctionConverter._
+import org.apache.spark.sql.sedona_sql.expressions.raster.implicits.RasterInputExpressionEnhancer
 import org.apache.spark.sql.sedona_sql.expressions.InferredExpression
+import org.geotools.coverage.grid.GridCoverage2D
 
 case class RS_BandNoDataValue(inputExpressions: Seq[Expression]) extends InferredExpression(inferrableFunction2(RasterBandAccessors.getBandNoDataValue), inferrableFunction1(RasterBandAccessors.getBandNoDataValue)) {
   protected def withNewChildrenInternal(newChildren: IndexedSeq[Expression]) = {
@@ -40,6 +45,28 @@ case class RS_Count(inputExpressions: Seq[Expression]) extends InferredExpressio
 case class RS_SummaryStats(inputExpressions: Seq[Expression]) extends InferredExpression(
   inferrableFunction1(RasterBandAccessors.getSummaryStats), inferrableFunction2(RasterBandAccessors.getSummaryStats),
   inferrableFunction3(RasterBandAccessors.getSummaryStats)) {
+  protected def withNewChildrenInternal(newChildren: IndexedSeq[Expression]) = {
+    copy(inputExpressions = newChildren)
+  }
+}
+
+case class RS_Band(inputExpressions: Seq[Expression]) extends InferredExpression(RasterBandAccessors.getBand _) {
+
+  override def evalWithoutSerialization(input: InternalRow): Any = {
+    val raster = inputExpressions.head.toRaster(input)
+    val intArray = inputExpressions(1).eval(input).asInstanceOf[ArrayData]
+    if (raster == null) {
+      null
+    } else {
+      val values = (0 until intArray.numElements()).map(i => intArray.getInt(i))
+      RasterBandAccessors.getBand(raster, values.toArray)
+    }
+  }
+
+  override def eval(input: InternalRow): Any = {
+    RasterUDT.serialize(evalWithoutSerialization(input).asInstanceOf[GridCoverage2D])
+  }
+
   protected def withNewChildrenInternal(newChildren: IndexedSeq[Expression]) = {
     copy(inputExpressions = newChildren)
   }
