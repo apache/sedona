21645e42d8698845eefcb2a6493eedba0e2d37c7
Furqaanahmed Khan <furqaankhan98@gmail.com>
2023-09-20 19:57:35 -0400

    feat: move to java, use InferredExpression, and test java for RS_GreaterThanEqual

--- common/src/main/java/org/apache/sedona/common/raster/MapAlgebra.java
+++ common/src/main/java/org/apache/sedona/common/raster/MapAlgebra.java
@@ -503,6 +503,25 @@ public class MapAlgebra
     }
 
     /**
+     * @param band band values
+     * @param target target to compare
+     * @return an array; mark all band values 1 that are greater than or equal to target, otherwise 0
+     */
+    public static double[] greaterThanEqual(double[] band, double target) {
+        double[] result = new double[band.length];
+
+        for (int i = 0; i < band.length; i++) {
+            if (band[i] >= target) {
+                result[i] = 1;
+            } else {
+                result[i] = 0;
+            }
+        }
+
+        return result;
+    }
+
+    /**
      * Throws an IllegalArgumentException if the lengths of the bands are not the same.
      * @param band1 length of band values
      * @param band2 length of band values
