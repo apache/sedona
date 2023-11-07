4c5fa8333b2c61850d5664b878df9493c7915066
Jia Yu <jiayu@apache.org>
2023-11-07 00:26:02 -0800

    [SEDONA-419] Remove SedonaKepler and SedonaPyDeck from `sedona.spark import *` (#1074)

--- python/sedona/spark/__init__.py
+++ python/sedona/spark/__init__.py
@@ -40,6 +40,4 @@ from sedona.utils import KryoSerializer
 from sedona.utils import SedonaKryoRegistrator
 from sedona.register import SedonaRegistrator
 from sedona.spark.SedonaContext import SedonaContext
-from sedona.maps.SedonaKepler import SedonaKepler
-from sedona.maps.SedonaPyDeck import SedonaPyDeck
 from sedona.raster_utils.SedonaUtils import SedonaUtils
