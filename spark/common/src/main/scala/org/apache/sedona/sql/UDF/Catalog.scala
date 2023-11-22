b0054ee93e44b4792b0a96227a7a8ace00d26499
Pranav Toggi <prantogg@gmail.com>
2023-11-22 13:33:37 -0500

    Update Catalog.scala

--- spark/common/src/main/scala/org/apache/sedona/sql/UDF/Catalog.scala
+++ spark/common/src/main/scala/org/apache/sedona/sql/UDF/Catalog.scala
@@ -230,6 +230,7 @@ object Catalog {
     function[RS_GeoTransform](),
     function[RS_PixelAsPoint](),
     function[RS_PixelAsPolygon](),
+    function[RS_PixelAsPolygons](),
     function[RS_PixelAsCentroid](),
     function[RS_Count](),
     function[RS_Clip](),
