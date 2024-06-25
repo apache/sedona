698dd6bc48c8c4e3c71ee3e51fba3fc7f99424ef
Pranav Toggi <prantogg@gmail.com>
2024-06-25 12:53:25 -0400

    Fix snowflake test

--- snowflake-tester/src/test/java/org/apache/sedona/snowflake/snowsql/TestFunctionsV2.java
+++ snowflake-tester/src/test/java/org/apache/sedona/snowflake/snowsql/TestFunctionsV2.java
@@ -1125,14 +1125,14 @@ public class TestFunctionsV2 extends TestBase {
     registerUDFV2("ST_Rotate", String.class, double.class);
     verifySqlSingleRes(
         "select ST_AsText(ST_ReducePrecision(sedona.ST_Rotate(ST_GeometryFromWKT('LINESTRING (0 0, 1 0, 1 1, 0 0)'), 10),2))",
-        "LINESTRING (0 0, -0.84 -0.54, -0.3 -1.38, 0 0)");
+        "LINESTRING(0 0, -0.84 -0.54, -0.3 -1.38, 0 0)");
     registerUDFV2("ST_Rotate", String.class, double.class, String.class);
     verifySqlSingleRes(
         "select ST_AsText(ST_ReducePrecision(sedona.ST_Rotate(ST_GeometryFromWKT('LINESTRING (0 0, 1 0, 1 1, 0 0)'), 10, ST_GeometryFromWKT('POINT (0 0)')),2))",
-        "LINESTRING (0 0, -0.84 -0.54, -0.3 -1.38, 0 0)");
+        "LINESTRING(0 0, -0.84 -0.54, -0.3 -1.38, 0 0)");
     registerUDFV2("ST_Rotate", String.class, double.class, double.class, double.class);
     verifySqlSingleRes(
         "select ST_AsText(ST_ReducePrecision(sedona.ST_Rotate(ST_GeometryFromWKT('LINESTRING (0 0, 1 0, 1 1, 0 0)'), 10, 0, 0),2))",
-        "LINESTRING (0 0, -0.84 -0.54, -0.3 -1.38, 0 0)");
+        "LINESTRING(0 0, -0.84 -0.54, -0.3 -1.38, 0 0)");
   }
 }
