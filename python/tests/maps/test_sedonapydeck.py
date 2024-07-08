dac381fcebe2993da4f77c93dfb0bdb7b37b9576
Furqaanahmed Khan <furqaankhan98@gmail.com>
2024-07-08 19:59:29 -0400

    feat: throw an error when column is not numeric and add stroked parameter

--- python/tests/maps/test_sedonapydeck.py
+++ python/tests/maps/test_sedonapydeck.py
@@ -30,6 +30,7 @@ class TestVisualization(TestBase):
         buildings_csv_df = self.spark.read.format("csv"). \
             option("delimiter", ","). \
             option("header", "true"). \
+            option("inferSchema", "true"). \
             load(google_buildings_input_location)
         buildings_csv_df.createOrReplaceTempView("buildings_table")
         buildings_df = self.spark.sql(
@@ -43,7 +44,7 @@ class TestVisualization(TestBase):
             auto_highlight=True,
             get_fill_color=fill_color,
             opacity=1.0,
-            stroked=False,
+            stroked=True,
             extruded=True,
             wireframe=True,
             get_elevation=0,
