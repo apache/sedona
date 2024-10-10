ec497f3b8ca57909e79cec1e3d051fc86bf6730f
John Bampton <jbampton@gmail.com>
2024-10-11 02:17:02 +1000

    [DOCS] Fix spelling and rename file

--- spark/common/src/main/java/org/apache/sedona/viz/extension/photoFilter/Emboss.java
+++ spark/common/src/main/java/org/apache/sedona/viz/extension/photoFilter/Emboss.java
@@ -22,11 +22,11 @@ import org.apache.sedona.viz.core.PhotoFilter;
 
 // TODO: Auto-generated Javadoc
 
-/** The Class Embose. */
-public class Embose extends PhotoFilter {
+/** The Class Emboss. */
+public class Emboss extends PhotoFilter {
 
-  /** Instantiates a new embose. */
-  public Embose() {
+  /** Instantiates a new emboss. */
+  public Emboss() {
     super(1);
     this.convolutionMatrix[0][0] = -2.0;
     this.convolutionMatrix[1][0] = -1.0;
