53c2dd6e01447258fdc4f6e64a5025abaf253b7a
Adam Binford <adamq43@gmail.com>
2024-12-11 07:06:13 -0500

    Add Spark 4 support

--- spark/common/src/main/scala/org/apache/spark/sql/sedona_sql/expressions/DataFrameAPI.scala
+++ spark/common/src/main/scala/org/apache/spark/sql/sedona_sql/expressions/DataFrameAPI.scala
@@ -22,51 +22,88 @@ import scala.reflect.ClassTag
 
 import org.apache.spark.sql.catalyst.expressions.{Expression, Literal}
 import org.apache.spark.sql.Column
+import org.apache.spark.sql.functions.lit
 import org.apache.spark.sql.expressions.UserDefinedAggregateFunction
 import org.apache.spark.sql.execution.aggregate.ScalaUDAF
 
 trait DataFrameAPI {
+
+  // The Column class changed in Spark 4, removing any direct usage of Expression
+  // Spark 3 Expression based methods:
+  val exprMethods =
+    try {
+      val exprMethod = Column.getClass().getDeclaredMethod("expr")
+      val constructor = Column.getClass().getDeclaredConstructor(classOf[Expression])
+      Some(exprMethod, constructor)
+    } catch {
+      case _: NoSuchMethodException => None
+    }
+
+  // Spark 4 method using Column.fn
+  val fnMethod =
+    try {
+      Some(Column.getClass().getDeclaredMethod("fn", classOf[String], classOf[Array[Column]]))
+    } catch {
+      case _: NoSuchMethodException => None
+    }
+
   protected def wrapExpression[E <: Expression: ClassTag](args: Any*): Column = {
-    val exprArgs = args.map(_ match {
-      case c: Column => c.expr
-      case s: String => Column(s).expr
-      case e: Expression => e
-      case x: Any => Literal(x)
-      case null => Literal(null)
-    })
-    val expressionConstructor =
-      implicitly[ClassTag[E]].runtimeClass.getConstructor(classOf[Seq[Expression]])
-    val expressionInstance = expressionConstructor.newInstance(exprArgs).asInstanceOf[E]
-    Column(expressionInstance)
+    wrapVarArgExpression[E](args)
   }
 
   protected def wrapVarArgExpression[E <: Expression: ClassTag](arg: Seq[Any]): Column = {
-    val exprArgs = arg.map(_ match {
-      case c: Column => c.expr
-      case s: String => Column(s).expr
-      case e: Expression => e
-      case x: Any => Literal(x)
-      case null => Literal(null)
-    })
-    val expressionConstructor =
-      implicitly[ClassTag[E]].runtimeClass.getConstructor(classOf[Seq[Expression]])
-    val expressionInstance = expressionConstructor.newInstance(exprArgs).asInstanceOf[E]
-    Column(expressionInstance)
+    val runtimeClass = implicitly[ClassTag[E]].runtimeClass
+    fnMethod
+      .map { fn =>
+        val colArgs = arg.map(_ match {
+          case c: Column => c
+          case s: String => Column(s)
+          case x: Any => lit(x)
+          case null => lit(null)
+        })
+        fn.invoke(runtimeClass.getSimpleName(), colArgs: _*).asInstanceOf[Column]
+      }
+      .getOrElse {
+        val (expr, constructor) = exprMethods.get
+        val exprArgs = arg.map(_ match {
+          case c: Column => expr.invoke(c).asInstanceOf[Expression]
+          case s: String => expr.invoke(Column(s)).asInstanceOf[Expression]
+          case e: Expression => e
+          case x: Any => Literal(x)
+          case null => Literal(null)
+        })
+        val expressionConstructor = runtimeClass.getConstructor(classOf[Seq[Expression]])
+        val expressionInstance = expressionConstructor.newInstance(exprArgs).asInstanceOf[E]
+        constructor.newInstance(expressionInstance).asInstanceOf[Column]
+      }
   }
 
   protected def wrapAggregator[A <: UserDefinedAggregateFunction: ClassTag](arg: Any*): Column = {
-    val exprArgs = arg.map(_ match {
-      case c: Column => c.expr
-      case s: String => Column(s).expr
-      case e: Expression => e
-      case x: Any => Literal(x)
-      case null => Literal(null)
-    })
-    val aggregatorClass = implicitly[ClassTag[A]].runtimeClass
-    val aggregatorConstructor = aggregatorClass.getConstructor()
-    val aggregatorInstance =
-      aggregatorConstructor.newInstance().asInstanceOf[UserDefinedAggregateFunction]
-    val scalaAggregator = ScalaUDAF(exprArgs, aggregatorInstance)
-    Column(scalaAggregator)
+    val runtimeClass = implicitly[ClassTag[A]].runtimeClass
+    fnMethod
+      .map { fn =>
+        val colArgs = arg.map(_ match {
+          case c: Column => c
+          case s: String => Column(s)
+          case x: Any => lit(x)
+          case null => lit(null)
+        })
+        fn.invoke(runtimeClass.getSimpleName(), colArgs: _*).asInstanceOf[Column]
+      }
+      .getOrElse {
+        val (expr, constructor) = exprMethods.get
+        val exprArgs = arg.map(_ match {
+          case c: Column => expr.invoke(c).asInstanceOf[Expression]
+          case s: String => expr.invoke(Column(s)).asInstanceOf[Expression]
+          case e: Expression => e
+          case x: Any => Literal(x)
+          case null => Literal(null)
+        })
+        val aggregatorConstructor = runtimeClass.getConstructor()
+        val aggregatorInstance =
+          aggregatorConstructor.newInstance().asInstanceOf[UserDefinedAggregateFunction]
+        val scalaAggregator = ScalaUDAF(exprArgs, aggregatorInstance)
+        constructor.newInstance(scalaAggregator).asInstanceOf[Column]
+      }
   }
 }
