340d0f79a4bcc85f623bc03ec5af82d8e896372a
pawelkocinski <pawel93kocinski@gmail.com>
2025-03-16 17:02:38 +0100

    SEDONA-721 Add docs.

--- python/tests/utils/test_pandas_arrow_udf.py
+++ python/tests/utils/test_pandas_arrow_udf.py
@@ -1,25 +1,40 @@
 from sedona.sql.types import GeometryType
-from sedona.sql.udf import sedona_vectorized_udf
+from sedona.sql.functions import sedona_vectorized_udf, SedonaUDFType
 from tests import chicago_crimes_input_location
 from tests.test_base import TestBase
 import pyspark.sql.functions as f
 import shapely.geometry.base as b
 from time import time
+import geopandas as gpd
 
 
 def non_vectorized_buffer_udf(geom: b.BaseGeometry) -> b.BaseGeometry:
-    return geom.buffer(0.001)
+    return geom.buffer(0.1)
 
 
-@sedona_vectorized_udf
+@sedona_vectorized_udf()
 def vectorized_buffer(geom: b.BaseGeometry) -> b.BaseGeometry:
-    return geom.buffer(0.001)
+    return geom.buffer(0.1)
+
+
+@sedona_vectorized_udf(udf_type=SedonaUDFType.GEO_SERIES)
+def vectorized_geo_series_buffer(series: gpd.GeoSeries) -> gpd.GeoSeries:
+    buffered = series.buffer(0.1)
+
+    return buffered
 
 
 buffer_distanced_udf = f.udf(non_vectorized_buffer_udf, GeometryType())
 
 
 class TestSedonaArrowUDF(TestBase):
+
+    def get_area(self, df, udf_fn):
+        return (
+            df.select(udf_fn(f.col("geom")).alias("buffer"))
+            .selectExpr("SUM(ST_Area(buffer))")
+            .collect()[0][0]
+        )
 
     def test_pandas_arrow_udf(self):
         df = (
@@ -32,15 +47,31 @@ class TestSedonaArrowUDF(TestBase):
         vectorized_times = []
         non_vectorized_times = []
 
-        for i in range(10):
+        for i in range(5):
             start = time()
-            df = df.withColumn("buffer", vectorized_buffer(f.col("geom")))
-            df.count()
+            area1 = self.get_area(df, vectorized_buffer)
+
+            assert area1 > 478
+
             vectorized_times.append(time() - start)
 
-            df = df.withColumn("buffer", buffer_distanced_udf(f.col("geom")))
-            df.count()
+            area2 = self.get_area(df, buffer_distanced_udf)
+
+            assert area2 > 478
+
             non_vectorized_times.append(time() - start)
 
         for v, nv in zip(vectorized_times, non_vectorized_times):
             assert v < nv, "Vectorized UDF is slower than non-vectorized UDF"
+
+    def test_geo_series_udf(self):
+        df = (
+            self.spark.read.option("header", "true")
+            .format("csv")
+            .load(chicago_crimes_input_location)
+            .selectExpr("ST_Point(y, x) AS geom")
+        )
+
+        area = self.get_area(df, vectorized_buffer)
+
+        assert area > 478
