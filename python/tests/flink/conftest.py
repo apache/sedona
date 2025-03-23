6720823b109c275051ea5faa1221f953c7dba22b
pawelkocinski <pawel93kocinski@gmail.com>
2025-03-23 22:53:20 +0100

    SEDONA-725 Add pyflink to Sedona.

--- python/tests/flink/conftest.py
+++ python/tests/flink/conftest.py
@@ -2,35 +2,38 @@ import os
 
 import pytest
 
-try:
-    from sedona.flink import SedonaContext
-except ImportError:
-    pass
-
-from pyflink.datastream import StreamExecutionEnvironment
-from pyflink.table import EnvironmentSettings, StreamTableEnvironment
-
 
 EXTRA_JARS = os.getenv("SEDONA_PYFLINK_EXTRA_JARS")
 
 
-@pytest.fixture(scope="module")
-def flink_settings():
-    return EnvironmentSettings.in_streaming_mode()
+def has_pyflink():
+    try:
+        import pyflink
+    except ImportError:
+        return False
+    return True
 
 
-@pytest.fixture(scope="module")
-def stream_env() -> StreamExecutionEnvironment:
-    env = StreamExecutionEnvironment.get_execution_environment()
-    jars = EXTRA_JARS.split(",") if EXTRA_JARS else []
-    for jar in jars:
-        env.add_jars(f"file://{jar}")
+if has_pyflink():
+    from sedona.flink import SedonaContext
+    from pyflink.datastream import StreamExecutionEnvironment
+    from pyflink.table import EnvironmentSettings, StreamTableEnvironment
 
-    return env
+    @pytest.fixture(scope="module")
+    def flink_settings():
+        return EnvironmentSettings.in_streaming_mode()
 
+    @pytest.fixture(scope="module")
+    def stream_env() -> StreamExecutionEnvironment:
+        env = StreamExecutionEnvironment.get_execution_environment()
+        jars = EXTRA_JARS.split(",") if EXTRA_JARS else []
+        for jar in jars:
+            env.add_jars(f"file://{jar}")
 
-@pytest.fixture(scope="module")
-def table_env(
-    stream_env: StreamExecutionEnvironment, flink_settings: EnvironmentSettings
-) -> StreamTableEnvironment:
-    return SedonaContext.create(stream_env, flink_settings)
+        return env
+
+    @pytest.fixture(scope="module")
+    def table_env(
+        stream_env: StreamExecutionEnvironment, flink_settings: EnvironmentSettings
+    ) -> StreamTableEnvironment:
+        return SedonaContext.create(stream_env, flink_settings)
