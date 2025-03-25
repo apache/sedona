e0663783bed4a2b5e79fe4a6786d53ee36cb6061
Dewey Dunnington <dewey@wherobots.com>
2025-03-25 11:11:54 -0500

    fix warning

--- spark/common/src/main/scala/org/apache/sedona/sql/datasources/arrow/ArrowSerializer.scala
+++ spark/common/src/main/scala/org/apache/sedona/sql/datasources/arrow/ArrowSerializer.scala
@@ -42,7 +42,6 @@ import org.apache.spark.sql.catalyst.encoders.AgnosticEncoders._
 import org.apache.spark.sql.catalyst.util.{SparkDateTimeUtils, SparkIntervalUtils}
 import org.apache.spark.sql.errors.ExecutionErrors
 import org.apache.spark.sql.types.Decimal
-import org.apache.spark.sql.util.ArrowUtils
 
 private[sql] trait CloseableIterator[E] extends Iterator[E] with AutoCloseable { self =>
   def asJava: java.util.Iterator[E] = new java.util.Iterator[E] with AutoCloseable {
