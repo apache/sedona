b00032463d126a64d2d692e3a0489c8bfc49c8b5
pawelkocinski <pawel93kocinski@gmail.com>
2025-05-01 21:50:55 +0200

    SEDONA-725 rearrange the spark module

--- python/sedona/stats/clustering/dbscan/__init__.py
+++ python/sedona/stats/clustering/dbscan/__init__.py
@@ -20,7 +20,7 @@ from sedona.spark.stats.clustering.dbscan import dbscan
 import warnings
 
 warnings.warn(
-    "The 'sedona.spark.stats.clustering.dbscan' module is deprecated and will be removed in future versions. Please use 'sedona.spark.stats' instead.",
+    "The 'sedona.stats.clustering.dbscan' module is deprecated and will be removed in future versions. Please use 'sedona.spark.stats' instead.",
     DeprecationWarning,
     stacklevel=2,
 )
