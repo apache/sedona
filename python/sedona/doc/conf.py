0899b25ebcfbd73639bf199d2ac0a19535a67dc6
zhangfengcdt <feng@wherobots.com>
2025-07-05 09:12:06 -0700

    [DOC] Add Sphinx Doc Build Framework to Sedona Python

---
+++ python/sedona/doc/conf.py
@@ -0,0 +1,40 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+import os
+import sys
+
+# Resolve the relative path to the `sedona` package
+sedona_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../'))
+
+# Add the path to the Python path
+sys.path.insert(0, sedona_path)
+
+project = 'Apache Sedona Python'
+copyright = '2025, Apache Software Foundation'
+author = 'Apache Software Foundation'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',  # For Google-style or NumPy-style docstrings
+]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
+
---
+++ python/sedona/doc/index.rst
@@ -0,0 +1,26 @@
+.. sedona-python documentation master file, created by
+   sphinx-quickstart on Sat Jul  5 08:38:02 2025.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+sedona-python documentation
+===========================
+
+Introduction to Sedona Python Packages
+======================================
+
+Apache Sedona provides a suite of Python packages for spatial data processing and analysis. These packages include modules for working with geospatial data, integrating with big data frameworks, and performing advanced spatial computations.
+
+The following documentation covers the available Python packages in Sedona, including their submodules and functionalities.
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   sedona.flink
+   sedona.geopandas
+   sedona.spark
+   sedona.stac
+   sedona.stats
+   sedona.utils
+
