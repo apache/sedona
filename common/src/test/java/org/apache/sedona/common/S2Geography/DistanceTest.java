8729657ce4c5b897b97e0deaf16188c409cfc57d
Zhuocheng Shang <122398181+ZhuochengShang@users.noreply.github.com>
2025-07-15 09:48:49 -0700

    [GH-2052] Implement geospatial functions of Distance, Predicates, Projection (#2051)
    * Create object of S2Geography, and implement PoinGeography with its encoder/decoder
    
    * Add POLYLINE implementation on S2Geography
    
    * Add POLYGON implements on S2Geography
    
    * Match coding style
    
    * "Apply Spotless formatting to PolylineGeographyTest"
    
    * Redesign of S2Geography
    
    - Import org.datasyslab s2-geometry-library
    - Clean up S2Geography abstract design
    - Update Encode/Decode inside each kind of geography
    
    * clean up unnecessary files in current branch
    
    * Refine design of EncodeTagged in S2Geography
    
    - Adding back EncodeTagged in S2Geography
    - Let each geography type calls its own encode / decode function
    - Change to use Kyro UnsafeInput and UnsafeOutput
    
    * Modify encoder() and add new test cases
    
    * clean up code of encode and clarify comments
    
    * Update POLYGON to only take one polygon
    
    * Remove S2Regionwrapper & S2Shapewrapper
    
    * clean up minor issue
    
    * GeographyCollection and ShapeIndexGeography implementation
    
    * comcomment geography kind in S2Geography
    
    * Functions of distance, predicates, projection & Accessors
    
    * fix minor issue of geospatial functions
    
    * format fix
    
    * avoid duplicates before create S2Loop; re init value list with initial value -1

---
+++ common/src/test/java/org/apache/sedona/common/S2Geography/DistanceTest.java
@@ -0,0 +1,96 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.sedona.common.S2Geography;
+
+import static org.junit.Assert.assertEquals;
+
+import com.google.common.geometry.*;
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.commons.lang3.tuple.Pair;
+import org.junit.Test;
+
+public class DistanceTest {
+  private static final double TOL = 1e-6;
+
+  @Test
+  public void pointDistance() throws Exception {
+    // 1) Build two points
+    S2Geography pt1 = new PointGeography(S2LatLng.fromDegrees(0, 0).toPoint());
+    S2Geography pt2 = new PointGeography(S2LatLng.fromDegrees(90, 0).toPoint());
+    ShapeIndexGeography geo1 = new ShapeIndexGeography(pt1);
+    ShapeIndexGeography geo2 = new ShapeIndexGeography(pt2);
+
+    Distance distance = new Distance();
+    double res = distance.S2_distance(geo1, geo2);
+    assertEquals(Math.PI / 2, res, 1e-9);
+  }
+
+  @Test
+  public void furtestDistance() throws Exception {
+    // 1) Build two points
+    S2Geography pt1 = new PointGeography(S2LatLng.fromDegrees(0, 0).toPoint());
+    S2Geography pt2 = new PointGeography(S2LatLng.fromDegrees(90, 0).toPoint());
+    ShapeIndexGeography geo1 = new ShapeIndexGeography(pt1);
+    ShapeIndexGeography geo2 = new ShapeIndexGeography(pt2);
+
+    Distance distance = new Distance();
+    double res = distance.S2_maxDistance(geo1, geo2);
+    assertEquals(Math.PI / 2, res, 1e-9);
+  }
+
+  @Test
+  public void testMinimumClearanceLineBetween() throws Exception {
+    S2Point pt = S2LatLng.fromDegrees(45, -64).toPoint();
+    S2Point pt_mid = S2LatLng.fromDegrees(45, 0).toPoint();
+    S2Point pt_end = S2LatLng.fromDegrees(0, 0).toPoint();
+    // Build a single polygon and wrap in geography
+    List<S2Point> points = new ArrayList<>();
+    points.add(pt);
+    points.add(pt_mid);
+    points.add(pt_end);
+    points.add(pt);
+    S2Loop polyline = new S2Loop(points);
+    S2Polygon poly = new S2Polygon(polyline);
+    PolygonGeography geo = new PolygonGeography(poly);
+
+    S2Point geo2 = S2LatLng.fromDegrees(45, 0).toPoint();
+    PointGeography geoPoint = new PointGeography(geo2);
+
+    ShapeIndexGeography geog1 = new ShapeIndexGeography();
+    geog1.addIndex(geo);
+    ShapeIndexGeography geog2 = new ShapeIndexGeography();
+    geog2.addIndex(geoPoint);
+
+    Distance distance = new Distance();
+    Pair<S2Point, S2Point> result = distance.S2_minimumClearanceLineBetween(geog1, geog2);
+
+    // Assert: both endpoints should lie at unit‐sphere direction (0.707107, 0, 0.707107)
+    S2Point expected = new S2Point(0.707107, 0.0, 0.707107);
+    S2Point actualA = result.getLeft();
+    S2Point actualB = result.getRight();
+    assertEquals(expected.getX(), actualA.getX(), TOL);
+    assertEquals(expected.getY(), actualA.getY(), TOL);
+    assertEquals(expected.getZ(), actualA.getZ(), TOL);
+
+    assertEquals(expected.getX(), actualB.getX(), TOL);
+    assertEquals(expected.getY(), actualB.getY(), TOL);
+    assertEquals(expected.getZ(), actualB.getZ(), TOL);
+  }
+}
