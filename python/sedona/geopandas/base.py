febc32845915e114fca0c680a364d709086d1676
Peter Nguyen <petern0408@gmail.com>
2025-07-26 17:28:32 -0700

    Properly delegate geodataframe.buffer(), remove non-implemented geoseries from geodataframe.py, remove all abstract method decorators in base.py

--- python/sedona/geopandas/base.py
+++ python/sedona/geopandas/base.py
@@ -47,10 +47,6 @@ class GeoFrame(metaclass=ABCMeta):
     A base class for both GeoDataFrame and GeoSeries.
     """
 
-    @abstractmethod
-    def __getitem__(self, key: Any) -> Any:
-        raise NotImplementedError("This method is not implemented yet.")
-
     def _reduce_for_geostat_function(
         self,
         sfun: Callable[["GeoSeries"], Column],
@@ -60,14 +56,6 @@ class GeoFrame(metaclass=ABCMeta):
         skipna: bool = True,
         **kwargs: Any,
     ) -> Union["GeoSeries", Scalar]:
-        raise NotImplementedError("This method is not implemented yet.")
-
-    @abstractmethod
-    def to_geopandas(self) -> Union[gpd.GeoDataFrame, pd.Series]:
-        raise NotImplementedError("This method is not implemented yet.")
-
-    @abstractmethod
-    def _to_geopandas(self) -> Union[gpd.GeoDataFrame, pd.Series]:
         raise NotImplementedError("This method is not implemented yet.")
 
     @property
@@ -94,7 +82,6 @@ class GeoFrame(metaclass=ABCMeta):
         # We pass in self.geometry here to use the active geometry column for dataframe
         return _delegate_to_geometry_column("sindex", self.geometry)
 
-    @abstractmethod
     def copy(self: GeoFrameLike) -> GeoFrameLike:
         raise NotImplementedError("This method is not implemented yet.")
 
@@ -120,17 +107,6 @@ class GeoFrame(metaclass=ABCMeta):
         dtype: float64
         """
         return _delegate_to_geometry_column("area", self)
-
-    # We need to implement these crs functions in the subclasses to avoid infinite recursion
-    @property
-    @abstractmethod
-    def crs(self):
-        raise NotImplementedError("This method is not implemented yet.")
-
-    @crs.setter
-    @abstractmethod
-    def crs(self, value):
-        raise NotImplementedError("This method is not implemented yet.")
 
     @property
     def geom_type(self):
@@ -158,7 +134,6 @@ class GeoFrame(metaclass=ABCMeta):
         return _delegate_to_geometry_column("geom_type", self)
 
     @property
-    @abstractmethod
     def type(self):
         raise NotImplementedError("This method is not implemented yet.")
 
@@ -306,15 +281,12 @@ class GeoFrame(metaclass=ABCMeta):
         """
         return _delegate_to_geometry_column("is_empty", self)
 
-    @abstractmethod
     def count_coordinates(self):
         raise NotImplementedError("This method is not implemented yet.")
 
-    @abstractmethod
     def count_geometries(self):
         raise NotImplementedError("This method is not implemented yet.")
 
-    @abstractmethod
     def count_interior_rings(self):
         raise NotImplementedError("This method is not implemented yet.")
 
@@ -348,17 +320,14 @@ class GeoFrame(metaclass=ABCMeta):
         return _delegate_to_geometry_column("is_simple", self)
 
     @property
-    @abstractmethod
     def is_ring(self):
         raise NotImplementedError("This method is not implemented yet.")
 
     @property
-    @abstractmethod
     def is_ccw(self):
         raise NotImplementedError("This method is not implemented yet.")
 
     @property
-    @abstractmethod
     def is_closed(self):
         raise NotImplementedError("This method is not implemented yet.")
 
@@ -394,7 +363,6 @@ class GeoFrame(metaclass=ABCMeta):
         """
         return _delegate_to_geometry_column("has_z", self)
 
-    @abstractmethod
     def get_precision(self):
         raise NotImplementedError("This method is not implemented yet.")
 
@@ -546,20 +514,16 @@ class GeoFrame(metaclass=ABCMeta):
         """
         return _delegate_to_geometry_column("centroid", self)
 
-    @abstractmethod
     def concave_hull(self, ratio=0.0, allow_holes=False):
         raise NotImplementedError("This method is not implemented yet.")
 
     @property
-    @abstractmethod
     def convex_hull(self):
         raise NotImplementedError("This method is not implemented yet.")
 
-    @abstractmethod
     def delaunay_triangles(self, tolerance=0.0, only_edges=False):
         raise NotImplementedError("This method is not implemented yet.")
 
-    @abstractmethod
     def voronoi_polygons(self, tolerance=0.0, extend_to=None, only_edges=False):
         raise NotImplementedError("This method is not implemented yet.")
 
@@ -605,53 +569,41 @@ class GeoFrame(metaclass=ABCMeta):
         """
         return _delegate_to_geometry_column("envelope", self)
 
-    @abstractmethod
     def minimum_rotated_rectangle(self):
         raise NotImplementedError("This method is not implemented yet.")
 
     @property
-    @abstractmethod
     def exterior(self):
         raise NotImplementedError("This method is not implemented yet.")
 
-    @abstractmethod
     def extract_unique_points(self):
         raise NotImplementedError("This method is not implemented yet.")
 
-    @abstractmethod
     def offset_curve(self, distance, quad_segs=8, join_style="round", mitre_limit=5.0):
         raise NotImplementedError("This method is not implemented yet.")
 
     @property
-    @abstractmethod
     def interiors(self):
         raise NotImplementedError("This method is not implemented yet.")
 
-    @abstractmethod
     def remove_repeated_points(self, tolerance=0.0):
         raise NotImplementedError("This method is not implemented yet.")
 
-    @abstractmethod
     def set_precision(self, grid_size, mode="valid_output"):
         raise NotImplementedError("This method is not implemented yet.")
 
-    @abstractmethod
     def representative_point(self):
         raise NotImplementedError("This method is not implemented yet.")
 
-    @abstractmethod
     def minimum_bounding_circle(self):
         raise NotImplementedError("This method is not implemented yet.")
 
-    @abstractmethod
     def minimum_bounding_radius(self):
         raise NotImplementedError("This method is not implemented yet.")
 
-    @abstractmethod
     def minimum_clearance(self):
         raise NotImplementedError("This method is not implemented yet.")
 
-    @abstractmethod
     def normalize(self):
         raise NotImplementedError("This method is not implemented yet.")
 
@@ -718,32 +670,25 @@ class GeoFrame(metaclass=ABCMeta):
             "make_valid", self, method=method, keep_collapsed=keep_collapsed
         )
 
-    @abstractmethod
     def reverse(self):
         raise NotImplementedError("This method is not implemented yet.")
 
-    @abstractmethod
     def segmentize(self, max_segment_length):
         raise NotImplementedError("This method is not implemented yet.")
 
-    @abstractmethod
     def transform(self, transformation, include_z=False):
         raise NotImplementedError("This method is not implemented yet.")
 
-    @abstractmethod
     def force_2d(self):
         raise NotImplementedError("This method is not implemented yet.")
 
-    @abstractmethod
     def force_3d(self, z=0):
         raise NotImplementedError("This method is not implemented yet.")
 
-    @abstractmethod
     def line_merge(self, directed=False):
         raise NotImplementedError("This method is not implemented yet.")
 
     @property
-    @abstractmethod
     def unary_union(self):
         raise NotImplementedError("This method is not implemented yet.")
 
@@ -2060,7 +2005,6 @@ class GeoFrame(metaclass=ABCMeta):
         """
         return _delegate_to_geometry_column("difference", self, other, align)
 
-    @abstractmethod
     def intersection_all(self):
         raise NotImplementedError("This method is not implemented yet.")
 
@@ -2176,15 +2120,12 @@ class GeoFrame(metaclass=ABCMeta):
         """
         return _delegate_to_geometry_column("contains", self, other, align)
 
-    @abstractmethod
     def contains_properly(self, other, align=None):
         raise NotImplementedError("This method is not implemented yet.")
 
-    @abstractmethod
     def to_parquet(self, path, **kwargs):
         raise NotImplementedError("This method is not implemented yet.")
 
-    @abstractmethod
     def buffer(
         self,
         distance,
@@ -2195,7 +2136,17 @@ class GeoFrame(metaclass=ABCMeta):
         single_sided=False,
         **kwargs,
     ):
-        raise NotImplementedError("This method is not implemented yet.")
+        return _delegate_to_geometry_column(
+            "buffer",
+            self,
+            distance,
+            resolution,
+            cap_style,
+            join_style,
+            mitre_limit,
+            single_sided,
+            **kwargs,
+        )
 
     def simplify(self, tolerance=None, preserve_topology=True):
         """Returns a ``GeoSeries`` containing a simplified representation of
@@ -2259,7 +2210,6 @@ class GeoFrame(metaclass=ABCMeta):
             "simplify", self, tolerance, preserve_topology
         )
 
-    @abstractmethod
     def sjoin(self, other, predicate="intersects", **kwargs):
         raise NotImplementedError("This method is not implemented yet.")
 
