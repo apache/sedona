7c93a9e2b01a1c97999f518b06b5b1ee08556495
Zhuocheng Shang <122398181+ZhuochengShang@users.noreply.github.com>
2025-08-08 16:18:47 -0700

    add test for ST_GeogFromWKB(byte array)

---
+++ common/src/test/java/org/apache/sedona/common/Geography/ConstructorsTest.java
@@ -0,0 +1,73 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.sedona.common.Geography;
+
+import static org.junit.Assert.assertEquals;
+
+import com.google.common.geometry.S2LatLng;
+import com.google.common.geometry.S2Point;
+import org.apache.sedona.common.S2Geography.Geography;
+import org.apache.sedona.common.S2Geography.SinglePointGeography;
+import org.apache.sedona.common.S2Geography.WKBWriter;
+import org.apache.sedona.common.geography.Constructors;
+import org.junit.Test;
+import org.locationtech.jts.io.ParseException;
+
+public class ConstructorsTest {
+
+  @Test
+  public void geogFromWKB() throws ParseException {
+    S2Point pt = S2LatLng.fromDegrees(45, -64).toPoint();
+    SinglePointGeography geog = new SinglePointGeography(pt);
+
+    // Test WKB without SRID
+    WKBWriter wkbWriter = new WKBWriter();
+    byte[] wkb = wkbWriter.write(geog);
+
+    Geography result = Constructors.geogFromWKB(wkb);
+    assertEquals(geog.toString(), result.toString());
+    assertEquals(0, result.getSRID());
+    assertEquals(0, result.getSRID());
+
+    // Test specifying SRID
+    result = Constructors.geogFromWKB(wkb, 1000);
+    assertEquals(geog.toString(), result.toString());
+    assertEquals(1000, result.getSRID());
+    assertEquals(1000, result.getSRID());
+
+    // Test EWKB with SRID
+    wkbWriter = new WKBWriter(2, true);
+    geog.setSRID(2000);
+    wkb = wkbWriter.write(geog);
+    result = Constructors.geogFromWKB(wkb);
+    assertEquals(geog.toString(), result.toString());
+    assertEquals(2000, result.getSRID());
+    assertEquals(2000, result.getSRID());
+
+    // Test overriding SRID
+    result = Constructors.geogFromWKB(wkb, 3000);
+    assertEquals(geog.toString(), result.toString());
+    assertEquals(3000, result.getSRID());
+    assertEquals(3000, result.getSRID());
+    result = Constructors.geogFromWKB(wkb, 0);
+    assertEquals(geog.toString(), result.toString());
+    assertEquals(0, result.getSRID());
+    assertEquals(0, result.getSRID());
+  }
+}
