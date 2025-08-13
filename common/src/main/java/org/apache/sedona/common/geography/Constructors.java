6809a7e60a1fbdd5e39ed79eab9ee94f66fd1b3c
Zhuocheng Shang <122398181+ZhuochengShang@users.noreply.github.com>
2025-08-12 18:08:59 -0700

    fix exception check for geography type

--- common/src/main/java/org/apache/sedona/common/geography/Constructors.java
+++ common/src/main/java/org/apache/sedona/common/geography/Constructors.java
@@ -58,16 +58,7 @@ public class Constructors {
         throw new ParseException("Invalid EWKT string");
       }
     }
-    try {
-      return geogFromWKT(wkt, SRID);
-    } catch (ParseException e) {
-      String msg = e.getMessage();
-      // Normalize JUST the common JTS phrasing
-      if (msg != null && msg.startsWith("Unknown geometry type:")) {
-        msg = msg.replaceFirst("geometry", "geography");
-      }
-      throw new ParseException(msg);
-    }
+    return geogFromWKT(wkt, SRID);
   }
 
   public static Geography geogCollFromText(String wkt, int srid) throws ParseException {
