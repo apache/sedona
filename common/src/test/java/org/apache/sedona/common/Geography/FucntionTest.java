eeb78d0a1cf0eb1fcd45fe6c6182c005e532f6fd
Zhuocheng Shang <122398181+ZhuochengShang@users.noreply.github.com>
2025-08-14 17:02:14 -0700

    example of split polygons at antimeridian

--- common/src/test/java/org/apache/sedona/common/Geography/FucntionTest.java
+++ common/src/test/java/org/apache/sedona/common/Geography/FucntionTest.java
@@ -37,4 +37,57 @@ public class FucntionTest {
     //        expect_equal(rect_multipoint$lng_lo, 179)
     //        expect_equal(rect_multipoint$lng_hi, -179)
   }
+
+  @Test
+  public void getEnvelopePoint() throws ParseException {
+    String wkt = "POINT (-180 10)";
+    Geography geography = Constructors.geogFromWKT(wkt, 0);
+    System.out.println(geography.toString());
+    Geography envelope = Functions.getEnvelope(geography);
+    System.out.println(envelope.toString());
+  }
+
+  @Test
+  public void getEnvelopeContry() throws ParseException {
+    String Netherlands =
+        "POLYGON ((3.314971 50.80372, 7.092053 50.80372, 7.092053 53.5104, 3.314971 53.5104, 3.314971 50.80372))";
+    Geography geography = Constructors.geogFromWKT(Netherlands, 4326);
+    Geography envelope = Functions.getEnvelope(geography);
+    System.out.println(envelope.toString());
+
+    //    <-------------------- WESTERN HEMISPHERE | EASTERN HEMISPHERE -------------------->
+    //
+    //            Longitude: ... -179.8°      -180°| 180°      177.3° ...
+    //    ----------------------------------+--------------------------------------------
+    //            |
+    //            Latitude                         |
+    //            -16°   +------------------------+ +------------------------+
+    //                   |                        | |                        |
+    //                   |       POLYGON 2        | |       POLYGON 1        |
+    //                   |                        | |                        |
+    //            -18.3° +------------------------+ +------------------------+
+    //                                             |
+    //                                             |
+    //                                             ^
+    //                                             |
+    //                                          Antimeridian
+    //                                    (The map's seam at 180°)
+
+    String Fiji =
+        "MULTIPOLYGON ( ( (177.285 -18.28799, 180 -18.28799, 180 -16.02088, 177.285 -16.02088, 177.285 -18.28799) ), ( (-180 -18.28799, -179.7933 -18.28799, -179.7933 -16.02088, -180 -16.02088, -180 -18.28799) ) )";
+    geography = Constructors.geogFromWKT(Fiji, 4326);
+    envelope = Functions.getEnvelope(geography);
+    System.out.println(envelope.toString());
+
+    String Antarctica = "POLYGON ((-180 -90, -180 -63.27066, 180 -63.27066, 180 -90, -180 -90))";
+    geography = Constructors.geogFromWKT(Antarctica, 4326);
+    envelope = Functions.getEnvelope(geography);
+    System.out.println(envelope.toString());
+
+    String multiCountry =
+        "MULTIPOLYGON (((-180 -90, -180 -63.27066, 180 -63.27066, 180 -90, -180 -90)),((3.314971 50.80372, 7.092053 50.80372, 7.092053 53.5104, 3.314971 53.5104, 3.314971 50.80372)))";
+    geography = Constructors.geogFromWKT(multiCountry, 4326);
+    envelope = Functions.getEnvelope(geography);
+    System.out.println(envelope.toString());
+  }
 }
