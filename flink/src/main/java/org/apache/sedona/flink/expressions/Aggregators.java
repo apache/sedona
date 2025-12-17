a43429b4ac62633fe24c9903297e828650f6de0a
Jia Yu <jiayu@apache.org>
2025-12-17 00:59:30 -0800

    Fix the aggregator according to new flink change

--- flink/src/main/java/org/apache/sedona/flink/expressions/Aggregators.java
+++ flink/src/main/java/org/apache/sedona/flink/expressions/Aggregators.java
@@ -244,12 +244,21 @@ public class Aggregators {
   }
 
   // Aliases for *_Aggr functions with *_Agg suffix
-  @DataTypeHint(value = "RAW", bridgedTo = Geometry.class)
+  @DataTypeHint(
+      value = "RAW",
+      rawSerializer = GeometryTypeSerializer.class,
+      bridgedTo = Geometry.class)
   public static class ST_Envelope_Agg extends ST_Envelope_Aggr {}
 
-  @DataTypeHint(value = "RAW", bridgedTo = Geometry.class)
+  @DataTypeHint(
+      value = "RAW",
+      rawSerializer = GeometryTypeSerializer.class,
+      bridgedTo = Geometry.class)
   public static class ST_Intersection_Agg extends ST_Intersection_Aggr {}
 
-  @DataTypeHint(value = "RAW", bridgedTo = Geometry.class)
+  @DataTypeHint(
+      value = "RAW",
+      rawSerializer = GeometryTypeSerializer.class,
+      bridgedTo = Geometry.class)
   public static class ST_Union_Agg extends ST_Union_Aggr {}
 }
