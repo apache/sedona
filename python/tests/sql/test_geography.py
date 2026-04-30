335b6fc25a99eaacedd454fa7dc59afd347b4f88
zhangfengcdt <fengzhang@apache.org>
2026-04-30 08:51:35 -0700

    fix pre-commit lint error

--- python/tests/sql/test_geography.py
+++ python/tests/sql/test_geography.py
@@ -61,9 +61,51 @@ class TestGeographyConstructorsDataFrameAPI(TestBase):
         # EWKB for SRID=4326; LINESTRING (-2.1 -0.4, -1.5 -0.7)
         ewkb_bytes = bytes(
             [
-                1, 2, 0, 0, 32, 230, 16, 0, 0, 2, 0, 0, 0,
-                0, 0, 0, 0, 132, 214, 0, 192, 0, 0, 0, 0, 128, 181, 214, 191,
-                0, 0, 0, 96, 225, 239, 247, 191, 0, 0, 0, 128, 7, 93, 229, 191,
+                1,
+                2,
+                0,
+                0,
+                32,
+                230,
+                16,
+                0,
+                0,
+                2,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                132,
+                214,
+                0,
+                192,
+                0,
+                0,
+                0,
+                0,
+                128,
+                181,
+                214,
+                191,
+                0,
+                0,
+                0,
+                96,
+                225,
+                239,
+                247,
+                191,
+                0,
+                0,
+                0,
+                128,
+                7,
+                93,
+                229,
+                191,
             ]
         )
         df = self.spark.createDataFrame([(ewkb_bytes,)], ["wkb"]).select(
@@ -94,9 +136,7 @@ class TestGeographyConstructorsDataFrameAPI(TestBase):
         assert wkt.startswith("POLYGON")
 
     def test_st_geogcoll_from_text(self):
-        wkt_in = (
-            "GEOMETRYCOLLECTION (POINT (1 2), LINESTRING (0 0, 1 1))"
-        )
+        wkt_in = "GEOMETRYCOLLECTION (POINT (1 2), LINESTRING (0 0, 1 1))"
         df = self.spark.sql(f"SELECT '{wkt_in}' AS wkt").select(
             stc.ST_GeogCollFromText(col("wkt"), lit(4326)).alias("g")
         )
@@ -131,9 +171,9 @@ class TestGeographyFunctionsDataFrameAPI(TestBase):
 
     def test_st_distance(self):
         df = self.spark.range(1).select(
-            stf.ST_Distance(
-                self._geog("POINT (0 0)"), self._geog("POINT (1 1)")
-            ).alias("d")
+            stf.ST_Distance(self._geog("POINT (0 0)"), self._geog("POINT (1 1)")).alias(
+                "d"
+            )
         )
         d = df.first()[0]
         assert 155000 < d < 160000  # ~157km on a sphere
@@ -153,9 +193,7 @@ class TestGeographyFunctionsDataFrameAPI(TestBase):
 
     def test_st_area(self):
         df = self.spark.range(1).select(
-            stf.ST_Area(
-                self._geog("POLYGON ((0 0, 1 0, 1 1, 0 1, 0 0))")
-            ).alias("a")
+            stf.ST_Area(self._geog("POLYGON ((0 0, 1 0, 1 1, 0 1, 0 0))")).alias("a")
         )
         a = df.first()[0]
         # 1°×1° box near equator on R=6371008m sphere ≈ 1.2364e10 m²
@@ -163,9 +201,9 @@ class TestGeographyFunctionsDataFrameAPI(TestBase):
 
     def test_st_centroid(self):
         df = self.spark.range(1).select(
-            stf.ST_Centroid(
-                self._geog("POLYGON ((0 0, 2 0, 2 2, 0 2, 0 0))")
-            ).alias("c")
+            stf.ST_Centroid(self._geog("POLYGON ((0 0, 2 0, 2 2, 0 2, 0 0))")).alias(
+                "c"
+            )
         )
         wkt = df.select(stf.ST_AsText(col("c"))).first()[0]
         assert wkt.startswith("POINT")
@@ -179,9 +217,9 @@ class TestGeographyFunctionsDataFrameAPI(TestBase):
 
     def test_st_envelope(self):
         df = self.spark.range(1).select(
-            stf.ST_Envelope(
-                self._geog("POLYGON ((0 0, 1 0, 1 1, 0 1, 0 0))")
-            ).alias("e")
+            stf.ST_Envelope(self._geog("POLYGON ((0 0, 1 0, 1 1, 0 1, 0 0))")).alias(
+                "e"
+            )
         )
         wkt = df.select(stf.ST_AsText(col("e"))).first()[0]
         assert wkt.startswith("POLYGON")
