611fc359ed7fecd99dc9e6a325eb1aca9dc84336
Jia Yu <jiayu@apache.org>
2026-07-24 08:32:50 -0700

    [GH-3156] Use valid polygon in explode parity test

--- python/tests/geopandas/test_match_geopandas_series.py
+++ python/tests/geopandas/test_match_geopandas_series.py
@@ -85,7 +85,7 @@ class TestMatchGeopandasSeries(TestGeopandasBase):
 
         self.polygons = [
             Polygon(),
-            Polygon([(0, 0), (1, 0), (2, 1), (3, 1)]),
+            Polygon([(0, 0), (1, 0), (2, 1), (0, 1)]),
             Polygon([(1, 1), (2, 1), (2, 2), (1, 2)]),
         ]
 
