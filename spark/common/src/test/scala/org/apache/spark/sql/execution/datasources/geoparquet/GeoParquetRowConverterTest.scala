0f26a29afa9583897d59856256631e5dd8087d32
Jia Yu <jiayu@apache.org>
2026-09-20 23:33:56 -0700

    Cover NaN dimensions in file reader tests

--- spark/common/src/test/scala/org/apache/spark/sql/execution/datasources/geoparquet/GeoParquetRowConverterTest.scala
+++ spark/common/src/test/scala/org/apache/spark/sql/execution/datasources/geoparquet/GeoParquetRowConverterTest.scala
@@ -24,21 +24,21 @@ import org.apache.parquet.schema.MessageTypeParser
 import org.apache.spark.sql.execution.datasources.geoparquet.internal.{LegacyBehaviorPolicy, NoopUpdater, RebaseSpec}
 import org.apache.spark.sql.sedona_sql.UDT.GeometryUDT
 import org.apache.spark.sql.types.{StructField, StructType}
-import org.locationtech.jts.geom.GeometryFactory
+import org.locationtech.jts.geom.{Coordinate, GeometryFactory, Point}
 import org.locationtech.jts.io.{Ordinate, WKBWriter}
 import org.scalatest.funsuite.AnyFunSuite
 
 import java.util.{Collections, EnumSet}
 
 class GeoParquetRowConverterTest extends AnyFunSuite {
-  test("primitive WKB retains declared empty coordinate layouts through GeometryUDT") {
+  private def primitiveConverter(): GeoParquetRowConverter = {
     val parquetSchema =
       MessageTypeParser.parseMessageType("message root { optional binary geometry; }")
     val catalystSchema = StructType(Seq(StructField("geometry", GeometryUDT(), nullable = true)))
     val metadata = Collections.singletonMap(
       "geo",
       """{"version":"1.1.0","primary_column":"geometry","columns":{"geometry":{"encoding":"WKB","geometry_types":[],"crs":null}}}""")
-    val converter = new GeoParquetRowConverter(
+    new GeoParquetRowConverter(
       new GeoParquetToSparkSchemaConverter(metadata, parameters = Map.empty),
       parquetSchema,
       catalystSchema,
@@ -47,6 +47,10 @@ class GeoParquetRowConverterTest extends AnyFunSuite {
       RebaseSpec(LegacyBehaviorPolicy.CORRECTED),
       Map.empty,
       NoopUpdater)
+  }
+
+  test("primitive WKB retains declared empty coordinate layouts through GeometryUDT") {
+    val converter = primitiveConverter()
     val factory = new GeometryFactory()
 
     Seq((3, 0), (3, 1), (4, 1)).foreach { case (dimension, measures) =>
@@ -71,6 +75,32 @@ class GeoParquetRowConverterTest extends AnyFunSuite {
         .getCoordinateSequence
       assert(resultSequence.getDimension == dimension)
       assert(resultSequence.getMeasures == measures)
+    }
+  }
+
+  test("primitive WKB retains NaN Z without carrying its layout into later XY rows") {
+    val converter = primitiveConverter()
+    val factory = new GeometryFactory()
+    val nanZ = new WKBWriter(3).write(factory.createPoint(new Coordinate(1, 2, 9)))
+    java.nio.ByteBuffer.wrap(nanZ).putDouble(21, Double.NaN)
+    val xy = new WKBWriter(2).write(factory.createPoint(new Coordinate(1, 2)))
+
+    Seq((nanZ, 3), (xy, 2), (nanZ, 3)).foreach { case (wkb, dimension) =>
+      converter.start()
+      converter
+        .getConverter(0)
+        .asPrimitiveConverter()
+        .addBinary(Binary.fromConstantByteArray(wkb))
+      converter.end()
+      val result =
+        GeometryUDT.deserialize(converter.currentRecord.getBinary(0)).asInstanceOf[Point]
+      val sequence = result.getCoordinateSequence
+      assert(!result.isEmpty)
+      assert(sequence.getDimension == dimension)
+      assert(sequence.getMeasures == 0)
+      assert(sequence.getX(0) == 1)
+      assert(sequence.getY(0) == 2)
+      if (dimension == 3) assert(sequence.getZ(0).isNaN)
     }
   }
 
